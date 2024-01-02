bedcea4056add5c700b5775b32ac79ff815afeed
Ankit Rana <ankitrana@Administrators-MacBook-Air-140.local>
2024-01-02 13:21:13 +0530

    Added logger

--- app/dao/labsConn.py
+++ app/dao/labsConn.py
@@ -2,6 +2,7 @@ from langchain.vectorstores.pgvector import PGVector
 from commonutils.utils import Singleton
 from torpedo import CONFIG
 from app.dao.openaiembedding import OpenAIEmbeddingsCustom
+from sanic.log import logger
 
 config = CONFIG.config
 
@@ -11,6 +12,10 @@ class LabsConn(metaclass=Singleton):
     def __init__(self):
         conn_str = config.get('DB_CONNECTION').get('CONNECTION_STRING')
         collection_name = config.get('DB_CONNECTION').get('LABS_COLLECTION')
+        logger.info("Collection_name: {}".format(collection_name))
+        logger.info(conn_str)
+        logger.info("Collection_name: {}, Collection_String_type: {}".format(type(collection_name), type(conn_str)))
+
         embeddings = OpenAIEmbeddingsCustom().get_openai_embeddings()
         store = PGVector(
             collection_name=collection_name,
