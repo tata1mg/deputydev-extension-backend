8d87d1780519922e48a075a47925aa9ef05cdf92
Ankit Rana <ankitrana@Administrators-MacBook-Air-140.local>
2024-01-03 00:14:03 +0530

    Remove hardcoded unavailable test

--- app/managers/openai_tools/openai_tools.py
+++ app/managers/openai_tools/openai_tools.py
@@ -13,7 +13,7 @@ async def show_lab_test_card(identifier: str, city: str) -> List[any]:
     @param identifier: The unique identifier of a test or Test ID
     @param city: The name of the city user is currently in or for whichever city user ask for in their question
     """
-    test_details = await LabsClient.get_lab_test_details('2558', city)
+    test_details = await LabsClient.get_lab_test_details(identifier, city)
     if not test_details:
         return [ChatTypeMsg.model_validate({"answer": "You can know more about TATA 1mg lab tests here "
                                                       "- https://1mg.com/labs"}),
