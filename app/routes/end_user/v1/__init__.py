b553e3592c9cb82925046c8be1a119830691105d
Vishal <vishal.khare@1mg.com>
2024-01-03 00:57:43 +0530

    Changing bot's name from diagnobot to jiva

--- app/routes/end_user/v1/__init__.py
+++ app/routes/end_user/v1/__init__.py
@@ -1,5 +1,5 @@
 from sanic.blueprints import Blueprint
 
-from app.routes.end_user.v1.diagnoBot import diagnoBot
+from app.routes.end_user.v1.jiva import jiva
 
-blueprints_v1 = Blueprint.group(diagnoBot, url_prefix="/v1/diagnobot")
+blueprints_v1 = Blueprint.group(jiva, url_prefix="/v1/jiva")
