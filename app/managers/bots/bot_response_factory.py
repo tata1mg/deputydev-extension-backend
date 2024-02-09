4da1256664f2f1aa605cedd02d5f943a75fb6d00
Ankit Rana <ankitrana@Administrators-MacBook-Air-140.local>
2024-02-09 22:50:58 +0530

    Added multimodality support

---
+++ app/managers/bots/bot_response_factory.py
@@ -0,0 +1,21 @@
+from app.models.chat import ChatModel
+from app.managers.bots.openai_response import OpenAiResponse
+from app.routes.end_user.headers import Headers
+from app.managers.bots.openai_response import OpenAiResponse
+from app.managers.bots.gemini_response import GeminiAiResponse
+from app.managers.bots.gemini_vision_response import GeminiVisionAiResponse
+
+
+class BotResponseFactory:
+    BOT_FACTORY = {
+        "diagnostics_query": OpenAiResponse,
+        "pdf": GeminiAiResponse,
+        "image": GeminiVisionAiResponse
+    }
+
+    @classmethod
+    async def get_bot_response(cls, payload: ChatModel.ChatRequestModel, headers):
+        kclass = cls.BOT_FACTORY.get(payload.chat_type)
+        response = await kclass().get_response(payload, headers)
+        return response
+
