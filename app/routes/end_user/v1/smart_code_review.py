9b989318a067a6e0977e84f6f37ab8e383b2e3dd
Vishal Khare <vishal.khare@1mg.com>
2024-03-23 00:42:40 +0530

    Committing some fixes in PR

--- app/routes/end_user/v1/smart_code_review.py
+++ app/routes/end_user/v1/smart_code_review.py
@@ -22,8 +22,8 @@ async def pool_assistance_api(_request: Request, **kwargs):
 @smart_code.route("/chat", methods=["POST"])
 async def chat_assistance_api(_request: Request, **kwargs):
     payload = _request.custom_json()
-    # TODO - This should moved to manager. Routes should act only as a proxy.
     headers = _request.headers
     request_id = headers.get("X-REQUEST-ID", "No request_id found")
+    # TODO - Unfulfilled parameter - comment
     await SmartCodeChatManager.chat(payload)
     return send_response(f"Processing Started with Request ID : {request_id}")
