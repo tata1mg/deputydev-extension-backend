77f83fcf7069c08e691f68a3dbc05e84bd87017e
Mohit Maheshwari <mohit.maheshwari@1mg.com>
2024-05-11 16:37:59 +0530

    changes in tokenizer module

--- app/modules/tokenizer/__init__.py
+++ app/modules/tokenizer/__init__.py
@@ -1,3 +1,3 @@
-from .tokenize import ContentTokenizer
+from .tokenize import ContentTokenizer, compute_document_tokens
 
-__all__ = ["ContentTokenizer"]
+__all__ = ["ContentTokenizer", "compute_document_tokens"]
