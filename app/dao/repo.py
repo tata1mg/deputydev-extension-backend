4b348d6b4f7a678556798a02c31f2d012950fc7a
Deepak <deepak.sharma1@1mg.com>
2024-05-30 14:16:04 +0530

    make issue id optional

--- app/dao/repo.py
+++ app/dao/repo.py
@@ -1,11 +1,12 @@
 from datetime import datetime
 
 from pydantic import BaseModel
+from typing import Optional
 
 
 class PullRequestResponse(BaseModel):
     title: str
     description: str
-    issue_id: str
+    issue_id: Optional[str]
     created_on: datetime
     updated_on: datetime
--- app/service_clients/jira/issue.py
+++ app/service_clients/jira/issue.py
@@ -8,12 +8,16 @@ class Issue(Base):
     ISSUE_PATH = "/issue"
 
     @classmethod
-    async def get(cls, issue_id, fields):
-        url = f"{cls.BASE_URL}{cls.V3_PATH}{cls.ISSUE_PATH}/{issue_id}"
-        query_params = {"fields": fields}
-        try:
-            response = requests.get(url, auth=cls.auth(), params=query_params, timeout=cls.TIMEOUT)
-            return response.json()
-        except Exception as e:
-            logger.error("Exception occured while fetching issue details from jira: {}".format(e))
+    async def get(cls, issue_id, fields=None):
+        if issue_id:
+            url = f"{cls.BASE_URL}{cls.V3_PATH}{cls.ISSUE_PATH}/{issue_id}"
+            query_params = {}
+            if fields: query_params['fields'] = fields
+            try:
+                response = requests.get(url, auth=cls.auth(), params=query_params, timeout=cls.TIMEOUT)
+                return response.json()
+            except Exception as e:
+                logger.error("Exception occured while fetching issue details from jira: {}".format(e))
+                return {}
+        else:
             return {}
