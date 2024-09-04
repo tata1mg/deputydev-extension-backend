29d113f4db39d70abf0d017e222ea8d0e2743ef2
Ankit Rana <ankit.rana1@1mg.com>
2024-09-04 06:12:56 +0000

    Merged in embeeding_logging_improvement (pull request #213)
    DD-310 Improved loggers while creating embedding
    
    * Improved loggers while creating embeeding
    
    * Fixed issue incase agent provides no empty comments
    
    
    Approved-by: Deepak Sharma (Technology)

--- app/main/blueprints/deputy_dev/services/code_review/agent_services/anthropic/anthropic_code_maintainability_agent.py
+++ app/main/blueprints/deputy_dev/services/code_review/agent_services/anthropic/anthropic_code_maintainability_agent.py
@@ -104,7 +104,7 @@ class AnthropicCodeMaintainabilityAgent(AgentServiceBase):
         <review>
         <comments>
         <comment>
-        <description>Describe the code maintainability issue</description>
+        <description>Describe the code maintainability issue and make sure to enclose description within <![CDATA[ ]]> to avoid XML parsing errors</description>
         <corrective_code>Rewrite or create new (in case of missing) code, docstring or documentation for
         developer to directly use it.
         Add this section under <![CDATA[ ]]> for avoiding xml paring error.
