24b38b1a14ecdae365a9cd4330d495ae11b2fdbc
Anand Raj <anand.raj@1mg.com>
2024-10-09 10:24:01 +0000

    Merged in DD-325-large-pr-message (pull request #277)
    DD-325 | large pr comment changed
    
    * large pr comment changed
    
    
    Approved-by: Deepak Sharma (Technology)

--- app/main/blueprints/deputy_dev/constants/constants.py
+++ app/main/blueprints/deputy_dev/constants/constants.py
@@ -9,10 +9,10 @@ MAX_PR_DIFF_TOKEN_LIMIT = CONFIG.config["MAX_PR_DIFF_TOKEN_LIMIT"]
 IGNORE_FILES = CONFIG.config["IGNORE_FILES"]
 COMMENTS_DEPTH = 7
 PR_SIZE_TOO_BIG_MESSAGE = (
-    "Ideal pull requests are not more than 50 lines. PRs with smaller diff are merged relatively "
-    "quickly and have much lower chances of getting reverted. This PR was found to have a diff token count "
-    "of {pr_diff_token_count} as a result of which it exceeded the max token count limit. SCRIT will only review PRs having total token count of not more than {max_token_limit}."
-    " Note :- Every 4 characters is equal to 1 token."
+    "This PR is too large. Ideal PRs are not more than 150-200 lines."
+    " Large PRs are harder to review and more likely to be rejected or "
+    "reverted. It is recommended to break down your changes into smaller, "
+    "more manageable pull requests."
 )
 BATCH_SIZE = CONFIG.config["BATCH_SIZE"]
 SCRIT_TAG = "#scrit"
