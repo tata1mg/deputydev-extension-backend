20decf4bc5495965e72ee1b24025250579257bc7
sachendra jain <sachendra.jain@1mg.com>
2024-12-11 19:33:52 +0530

    settings

--- app/main/blueprints/deputy_dev/services/code_review/agent_services/agent_factory.py
+++ app/main/blueprints/deputy_dev/services/code_review/agent_services/agent_factory.py
@@ -23,6 +23,14 @@ from app.main.blueprints.deputy_dev.services.code_review.agent_services.openai.o
 from app.main.blueprints.deputy_dev.services.code_review.context.context_service import (
     ContextService,
 )
+from app.main.blueprints.deputy_dev.services.workspace.context_vars import (
+    get_context_value,
+)
+from app.main.blueprints.deputy_dev.services.code_review.agent_services.agent_base import (
+    AgentServiceBase,
+)
+from app.main.blueprints.deputy_dev.constants.constants import TokenTypes
+from copy import deepcopy
 
 
 class AgentFactory:
@@ -39,6 +47,7 @@ class AgentFactory:
     def __init__(self, reflection_enabled: bool, context_service: ContextService):
         self.context_service = context_service
         self.reflection_enabled = reflection_enabled
+        self.factories = deepcopy(self.FACTORIES)
 
     async def build_prompts(self, reflection_stage, previous_review_comments, exclude_agents):
         prompts = {}
@@ -62,3 +71,45 @@ class AgentFactory:
             "confluence_doc_id": self.context_service.confluence_id,
         }
         return prompts, meta_info
+
+    def initialize_custom_agents(self):
+        all_agents = get_context_value("setting")["code_review_agents"]["agents"]
+        for agent_name, agent_setting in all_agents.itmes():
+            if agent_setting["is_custom_agent"] and agent_setting["enable"]:
+                agent_class = self.create_custom_agent(agent_name, self.context_service, self.reflection_enabled)
+                self.factories[agent_name] = agent_class
+
+    def create_custom_agent(self, agent_name, context_service, is_reflection_enabled):
+        def init_method(self, *args, **kwargs):
+            super().__init__(context_service, is_reflection_enabled, agent_name)
+
+        def get_with_reflection_system_prompt_pass1(self):
+            return ""
+
+        def get_with_reflection_user_prompt_pass1(self):
+            return ""
+
+        def get_with_reflection_system_prompt_pass2(self):
+            return ""
+
+        def get_with_reflection_user_prompt_pass2(self):
+            return ""
+
+        def get_agent_specific_tokens_data(self):
+            return {
+                TokenTypes.PR_TITLE.value: self.context_service.pr_title_tokens,
+                TokenTypes.PR_DESCRIPTION.value: self.context_service.pr_description_tokens,
+                TokenTypes.PR_DIFF_TOKENS.value: self.context_service.pr_diff_tokens,
+                TokenTypes.RELEVANT_CHUNK.value: self.context_service.embedding_input_tokens,
+            }
+
+        functions = {
+            "__init__": init_method,
+            "get_with_reflection_system_prompt_pass1": get_with_reflection_system_prompt_pass1,
+            "get_with_reflection_user_prompt_pass1": get_with_reflection_user_prompt_pass1,
+            "get_with_reflection_system_prompt_pass2": get_with_reflection_system_prompt_pass2,
+            "get_with_reflection_user_prompt_pass2": get_with_reflection_user_prompt_pass2,
+            "get_agent_specific_tokens_data": get_agent_specific_tokens_data,
+        }
+        # Dynamically create and return the class
+        return type(agent_name, (AgentServiceBase,), functions)
