10e85956e6fe7d2ab70be5b212a240d5cc910e26
Sachendra Jain <sachendra.jain@1mg.com>
2024-12-18 10:22:40 +0000

    Merged in setting_file_name_update (pull request #375)
    DD-337 file name changed
    
    * file name changed
    
    * setting file name update
    
    * setting logic updated
    
    
    Approved-by: Deepak Sharma (Technology)

--- app/main/blueprints/deputy_dev/services/setting_service.py
+++ app/main/blueprints/deputy_dev/services/setting_service.py
@@ -189,15 +189,14 @@ class SettingService:
         await RepoSettingCache.set(cache_key, repo_level_settings)
         if repo_level_settings in [-1, -2]:  # Save in DB
             repo_level_settings = None
-        if error or repo_level_settings:
-            repo = await self.repo_service.fetch_repo()
-            if repo:
-                await self.create_or_update_setting(
-                    configurable_id=repo.id,
-                    configurable_type=SettingLevel.REPO.value,
-                    error=error,
-                    setting=repo_level_settings,
-                )
+        repo = await self.repo_service.fetch_repo()
+        if repo:
+            await self.create_or_update_setting(
+                configurable_id=repo.id,
+                configurable_type=SettingLevel.REPO.value,
+                error=error,
+                setting=repo_level_settings,
+            )
 
     def repo_setting_cache_key(self):
         scm_workspace_id = self.repo_service.workspace_id
@@ -233,20 +232,23 @@ class SettingService:
 
     @classmethod
     async def create_or_update_setting(cls, configurable_id, configurable_type, error, setting):
-        if error or setting:
-            payload = {
-                "configurable_id": configurable_id,
-                "configurable_type": configurable_type,
-                "configuration": setting,
-                "error": error,
-            }
-            saved_setting = await Configurations.get_or_none(
-                configurable_id=configurable_id, configurable_type=configurable_type
-            )
-            if not saved_setting:
+        payload = {
+            "configurable_id": configurable_id,
+            "configurable_type": configurable_type,
+            "configuration": setting,
+            "error": error,
+        }
+        saved_setting = await Configurations.get_or_none(
+            configurable_id=configurable_id, configurable_type=configurable_type
+        )
+        if not saved_setting:
+            if error or setting:
                 setting = Configurations(**payload)
                 await setting.save()
-            else:
+        else:
+            if error or setting:
                 payload["id"] = saved_setting.id
                 setting = Configurations(**payload)
                 await setting.save(update_fields=["configuration", "error"], force_update=True)
+            else:
+                await saved_setting.delete()
