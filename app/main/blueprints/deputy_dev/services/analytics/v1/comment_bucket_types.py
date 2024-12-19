93c4f0949146427e34d9ee43437cd4f6b06de712
pratham.verma <pratham.verma@1mg.com>
2024-12-19 05:16:24 +0000

    Merged in DD_dashboard (pull request #305)
    DD-359 dashboard backend apis
    
    * pydantic model added in models for dashboard analytics data used in open api definition
    
    * black fix
    
    * merge rate format fixed
    
    * comment bucket types data formatted, removing underscore and converting bucket type value into title
    
    * black fix
    
    * deputy dev replica db connection added in config template
    
    * Merge branch 'master' of bitbucket.org:tata1mg/genai into DD_dashboard
    
    * rejected status types moved to constants from serializer constants
    
    * black fix
    
    * tiles route name fix
    
    * analytics factory class will return instance
    
    * code review time query scm close time bug fix
    
    * exceptions handled correctly and db migration added for index adding
    
    * blueprint fix, using console in place of dashboard data
    
    * Merged master into DD_dashboard
    
    * deeplink for github fix and teams query change fetching all teams
    
    * Merge branch 'DD_dashboard' of bitbucket.org:tata1mg/genai into DD_dashboard
    
    * now serializer factory class only return instance
    
    * Merged master into DD_dashboard
    
    * extra indexes removed and chat request move to background in starting itself
    
    * Merge branch 'master' of bitbucket.org:tata1mg/genai into DD_dashboard
    
    * black fix
    
    * Merge branch 'DD_dashboard' of bitbucket.org:tata1mg/genai into DD_dashboard
    
    * hard coded prs serializer fix
    
    * black fix
    
    
    Approved-by: Anand Raj
    Approved-by: Deepak Sharma (Technology)

---
+++ app/main/blueprints/deputy_dev/services/analytics/v1/comment_bucket_types.py
@@ -0,0 +1,43 @@
+from app.main.blueprints.deputy_dev.constants.dashboard_constants import (
+    AnalyticsDataQueries,
+    StatusTypes,
+)
+
+from .base_analytics import BaseAnalytics
+
+
+class CommentBucketTypes(BaseAnalytics):
+    """Analytics class for generating data about comment types."""
+
+    def __init__(
+        self,
+        start_date: str,
+        end_date: str,
+        repo_ids: str = None,
+        graph_type: str = None,
+        interval_filter: str = None,
+    ):
+        super().__init__(
+            start_date=start_date,
+            end_date=end_date,
+            repo_ids=repo_ids,
+            graph_type=graph_type,
+            interval_filter=interval_filter,
+        )
+        self.graph_type = "comment_types"
+
+    def format_query(self) -> str:
+        """
+        Formats the SQL query to fetch data about comment types.
+
+        Returns:
+            str: SQL query string
+        """
+        base_query = AnalyticsDataQueries.comment_bucket_types_query.value.format(
+            start_date=self.start_date,
+            end_date=self.end_date,
+            repo_ids=self.repo_ids,
+            bucket_status=StatusTypes.BUCKET_STATUS.value,
+        )
+
+        return base_query
---
+++ app/main/blueprints/deputy_dev/services/serializers/shadcn/comment_bucket_types.py
@@ -0,0 +1,31 @@
+from app.main.blueprints.deputy_dev.services.serializers.shadcn.base_shadcn import (
+    BaseShadcn,
+)
+
+
+class CommentBucketTypes(BaseShadcn):
+    """
+    A serializer class for processing raw comment data.
+
+    This class inherits from the BaseShadcn and provides a specific implementation
+    for processing raw data related to comment types.
+
+    Attributes:
+        raw_data: The raw input data that needs to be processed.
+    """
+
+    def process_raw_data(self):
+        """
+        Process and return the raw data with formatted bucket types.
+
+        Returns:
+            The raw data with bucket types formatted.
+        """
+        formatted_data = []
+        for item in self.raw_data:
+            formatted_item = item.copy()  # Copy the original item
+            # Format the bucket_type
+            # Removing the underscore and formatting it into title
+            formatted_item["bucket_type"] = formatted_item["bucket_type"].replace("_", " ").title()
+            formatted_data.append(formatted_item)
+        return formatted_data
