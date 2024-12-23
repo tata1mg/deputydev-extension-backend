2cfe7b22e7a86a8200e125f649b19d4760f8c90a
sachendra jain <sachendra.jain@1mg.com>
2024-12-23 14:43:46 +0530

    repo setting update

--- app/main/blueprints/deputy_dev/models/dao/agents.py
+++ app/main/blueprints/deputy_dev/models/dao/agents.py
@@ -5,8 +5,8 @@ import uuid
 
 class Agents(Base):
     id = fields.BigIntField(pk=True)
-    agent_id = fields.UUIDField(default=uuid.uuid4)
-    repo_id = fields.CharField(max_length=100, null=False)
+    agent_id = fields.UUIDField()
+    repo_id = fields.BigIntField(null=False)
     display_name = fields.CharField(max_length=100, null=False)
     agent_name = fields.CharField(max_length=100, null=False)
 
