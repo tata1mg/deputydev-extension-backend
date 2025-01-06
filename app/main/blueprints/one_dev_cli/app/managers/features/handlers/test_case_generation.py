111194e48fdd123939634b86f599cf984feff179
Satyam Kumar <satyam.kumar1@1mg.com>
2025-01-06 18:56:56 +0530

    DD-376 - Fixes

--- app/main/blueprints/one_dev_cli/app/managers/features/handlers/test_case_generation.py
+++ app/main/blueprints/one_dev_cli/app/managers/features/handlers/test_case_generation.py
@@ -73,13 +73,12 @@ class TestCaseGenerationHandler(BaseFeatureHandler):
             )
 
         selected_text = self._get_selected_text(self.query).get_xml()
-        query = selected_text + "   \n  " + self.query.custom_instructions if self.query.custom_instructions else ""
+        query = selected_text + ("   \n  " + self.query.custom_instructions if self.query.custom_instructions else "")
         final_payload["query"] = selected_text
         if self.query.custom_instructions:
             final_payload["custom_instructions"] = self.query.custom_instructions
 
         query_vector = await self.embedding_manager.embed_text_array(texts=[query], store_embeddings=False)
-        final_payload["query"] = query
         relevant_chunks, _ = await ChunkingManger.get_relevant_chunks(
             query=query,
             local_repo=self.local_repo,
