8b0acf0341549c3783befc8b672d5d507299de52
Pratham verma <pratham.verma@1mg.com>
2025-01-18 22:58:41 +0530

    cli auth implemented

--- app/main/blueprints/deputydev_cli/app/ui/helpers/feature_runner.py
+++ app/main/blueprints/deputydev_cli/app/ui/helpers/feature_runner.py
@@ -39,7 +39,6 @@ class FeatureRunner:
             and self.app_context.embedding_manager is not None
             and self.app_context.chunkable_files_with_hashes is not None
             and self.app_context.auth_token is not None
-            and self.app_context.local_user_details is not None
             and self.app_context.process_executor is not None
             and self.app_context.usage_hash is not None
         ):
@@ -47,7 +46,6 @@ class FeatureRunner:
                 asyncio.create_task(
                     FeatureFactory.handle_feature(
                         process_executor=self.app_context.process_executor,
-                        local_user_details=self.app_context.local_user_details,
                         feature=self.app_context.operation,
                         query=self.app_context.query,
                         one_dev_client=self.app_context.one_dev_client,
--- app/main/blueprints/one_dev/routes/end_user/v1/auth.py
+++ app/main/blueprints/one_dev/routes/end_user/v1/auth.py
@@ -1,7 +1,8 @@
 from sanic import Blueprint
 from torpedo import Request, send_response
 
-from app.main.blueprints.one_dev.services.auth.login_by_team import TeamLogin
+from app.backend_common.services.supabase.session import SupabaseSession
+from app.main.blueprints.one_dev.services.auth.login import Login
 from app.main.blueprints.one_dev.utils.pre_authenticate_handler import (
     validate_cli_version,
 )
@@ -12,6 +13,13 @@ auth = Blueprint("auth", "/")
 @auth.route("/verify-auth-token", methods=["POST"])
 @validate_cli_version
 async def verify_auth_token(_request: Request, **kwargs):
-    payload = _request.custom_json()
-    response = await TeamLogin.verify_auth_token(**payload)
+    headers = _request.headers
+    response = await Login.verify_auth_token(headers)
+    return send_response(response)
+
+@auth.route("/get-session", methods=["GET"])
+@validate_cli_version
+async def get_session(_request: Request, **kwargs):
+    headers = _request.headers
+    response = await SupabaseSession.get_session_by_device_code(headers)
     return send_response(response)
