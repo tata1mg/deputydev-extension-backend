969d61416ecb41f63cc9b1d608d6ee7b3d4d2b26
Pratham verma <pratham.verma@1mg.com>
2025-01-19 19:35:02 +0530

    register user and register user team apis created

--- app/backend_common/models/dto/user_team_dto.py
+++ app/backend_common/models/dto/user_team_dto.py
@@ -9,7 +9,7 @@ class UserTeamDTO(BaseModel):
     user_id: int
     team_id: int
     role: str
-    last_pr_authored_or_reviewed_at: Optional[datetime]
+    last_pr_authored_or_reviewed_at: Optional[datetime] = None
     is_owner: bool
     is_billable: bool
     created_at: Optional[datetime] = None
