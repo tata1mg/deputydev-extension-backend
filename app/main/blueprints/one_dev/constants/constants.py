3234af8b057eac6e4228ca33ccace228a541ce17
Pratham verma <pratham.verma@1mg.com>
2025-01-22 09:20:34 +0530

    black fix

--- app/main/blueprints/one_dev/constants/constants.py
+++ app/main/blueprints/one_dev/constants/constants.py
@@ -1,13 +1,5 @@
 DEPRECATED_CLI_VERSIONS = {}
 
-TATA_1MG = {
-    "domain": "1mg.com",
-    "team_id": 1,
-    "org_name": "TATA 1mg"
-}
+TATA_1MG = {"domain": "1mg.com", "team_id": 1, "org_name": "TATA 1mg"}
 
-TRAYA = {
-    "domain": "traya.com",
-    "team_id": 3,
-    "org_name": "Traya"
-}
+TRAYA = {"domain": "traya.com", "team_id": 3, "org_name": "Traya"}
