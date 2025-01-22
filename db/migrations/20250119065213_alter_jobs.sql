90a80cbb50217c15267aaf653ed0ecf7579a25ea
Pratham verma <pratham.verma@1mg.com>
2025-01-22 09:17:29 +0530

    now storing user team id in job table

--- db/migrations/20250119065213_alter_jobs.sql
+++ db/migrations/20250119065213_alter_jobs.sql
@@ -3,11 +3,13 @@ delete from job;
 alter table job drop column advocacy_id;
 alter table job drop column user_email;
 alter table job drop column user_name;
-alter table job add column user_id bigint references users(id) not null;
+alter table job drop column team_id;
+alter table job add column user_team_id bigint references user_teams(id) not null;
 
 
 -- migrate:down
-alter table job drop column user_id;
+alter table job drop column user_team_id;
 alter table job add column advocacy_id int not null;
 alter table job add column user_email varchar;
 alter table job add column user_name varchar;
+alter table job add column team_id int not null;
