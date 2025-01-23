19f37749d63e9db24f8b034bcdbb8e69ee0be0d5
Satyam Kumar <satyam.kumar1@1mg.com>
2025-01-23 17:29:44 +0530

    Refactorings

--- app/main/blueprints/deputydev_cli/app/clients/constants.py
+++ app/main/blueprints/deputydev_cli/app/clients/constants.py
@@ -2,5 +2,5 @@ LIMIT = 0
 LIMIT_PER_HOST = 0
 TTL_DNS_CACHE = 10
 TIMEOUT = 100
-HOST = "https://api.deputydev.ai"
-# HOST = "http://localhost:8084"
+# HOST = "https://api.deputydev.ai"
+HOST = "http://localhost:8084"
