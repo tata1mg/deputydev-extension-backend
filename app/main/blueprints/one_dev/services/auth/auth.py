43942ce91e83ba3b3cd0fd97ddebeaf4015050fd
Pratham verma <pratham.verma@1mg.com>
2025-01-23 19:15:57 +0530

    now passing directly jwt for verifying

---
+++ app/main/blueprints/one_dev/services/auth/auth.py
@@ -0,0 +1,15 @@
+from typing import Any, Dict
+
+from app.main.blueprints.one_dev.services.auth.login import Login
+
+class Auth:
+    @classmethod
+    async def extract_and_verify_token(cls, headers: Dict[str, str]) -> Dict[str, Any]:
+        authorization_header = headers.get("Authorization")
+        if not authorization_header:
+            raise Exception("Authorization header is missing")
+
+        # decode the JWT token and get the supabase access token
+        jwt = authorization_header.split(" ")[1]
+        result = await Login.verify_auth_token(jwt)
+        return result
