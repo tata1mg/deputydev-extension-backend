c69bf45e3f5bac6df64da3e34138203b8134d2c2
Pratham verma <pratham.verma@1mg.com>
2025-01-27 12:02:11 +0530

    now using torpedo config in place of config manager

--- app/main/blueprints/one_dev/services/auth/login.py
+++ app/main/blueprints/one_dev/services/auth/login.py
@@ -4,14 +4,14 @@ from torpedo.exceptions import BadRequestException
 
 from app.backend_common.services.auth.supabase.auth import SupabaseAuth
 from app.common.services.authentication.jwt import JWTHandler
-from app.common.utils.config_manager import ConfigManager
+from torpedo import CONFIG
 
 
 class Login:
     @classmethod
     async def verify_auth_token(cls, jwt_token: str) -> Dict[str, Any]:
         try:
-            session_data = JWTHandler(signing_key=ConfigManager.config["JWT_SECRET_KEY"]).verify_token(jwt_token)
+            session_data = JWTHandler(signing_key=CONFIG.config["JWT_SECRET_KEY"]).verify_token(jwt_token)
             access_token = session_data.get("access_token")
             response = await SupabaseAuth.verify_auth_token(access_token)
             if not response["valid"]:
