d30fb25d8d4fd2c1ec2fdaf0258198e553f71d57
Satyam Kumar <satyam.kumar1@1mg.com>
2025-01-30 22:40:38 +0530

    Changes for config

--- app/main/blueprints/deputydev_cli/app/ui/__init__.py
+++ app/main/blueprints/deputydev_cli/app/ui/__init__.py
@@ -1,6 +1,5 @@
 import asyncio
 import sys
-import traceback
 from typing import Dict, Optional
 
 from app.common.utils.config_manager import ConfigManager
@@ -9,17 +8,23 @@ from app.main.blueprints.deputydev_cli.app.exceptions.exceptions import (
     InvalidVersionException,
 )
 
+from app.main.blueprints.deputydev_cli.app.ui.screens.dataclasses.main import AppContext, ScreenType
 
-async def close_session_and_exit(one_dev_client: OneDevClient):
+
+one_dev_client = OneDevClient()
+app_context = AppContext(one_dev_client=one_dev_client)
+
+
+async def close_session_and_exit():
     print("Exiting ...")
+    global one_dev_client
     await one_dev_client.close_session()
     sys.exit(0)
 
 
-async def populate_config():
-    one_dev_client = OneDevClient()
-
+async def populate_essential_config():
     # get the configs
+    global one_dev_client
     try:
         ConfigManager.initialize(in_memory=True)
         configs: Optional[Dict[str, str]] = await one_dev_client.get_essential_configs(
@@ -30,13 +35,45 @@ async def populate_config():
         ConfigManager.set(configs)
     except InvalidVersionException:
         print("This CLI client is not supported anymore")
-        await close_session_and_exit(one_dev_client)
     except Exception:
         print("Failed to fetch configs")
-        print(traceback.format_exc())
-        await close_session_and_exit(one_dev_client)
 
+
+async def populate_full_config():
+    # get the configs
+    global one_dev_client
+    global app_context
+    try:
+        configs: Optional[Dict[str, str]] = await one_dev_client.get_configs(
+            headers={"Content-Type": "application/json", "Authorization": f"Bearer {app_context.auth_token}"}
+        )
+        if configs is None:
+            raise Exception("No configs fetched")
+        ConfigManager.set(configs)
+    except Exception:
+        print("Failed to fetch configs")
+
+
+async def initialize_cli_ui():
+    # get the essential configs
+    await populate_essential_config()
+
+    # authenticate the user
+    global app_context
+    global one_dev_client
+    from app.main.blueprints.deputydev_cli.app.ui.screens.authentication import (
+        Authentication,
+    )
+    updated_app_context, redirect_screen = await Authentication(app_context).render()
+
+    if redirect_screen == ScreenType.EXIT or redirect_screen == ScreenType.HOME:
+        await close_session_and_exit()
+
+    app_context = updated_app_context
+
+    # update the configs
+    await populate_full_config()
     await one_dev_client.close_session()
 
 
-asyncio.run(populate_config())
+asyncio.run(initialize_cli_ui())
