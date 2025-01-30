31f37fbf74483ecad920a2cbb2d513a2c1e0833d
Satyam Kumar <satyam.kumar1@1mg.com>
2025-01-30 23:52:31 +0530

    Formatting changes

--- app/main/blueprints/deputydev_cli/app/ui/screens/authentication.py
+++ app/main/blueprints/deputydev_cli/app/ui/screens/authentication.py
@@ -3,7 +3,9 @@ from typing import Any, Dict, Tuple
 from prompt_toolkit import PromptSession
 
 from app.common.utils.app_logger import AppLogger
-from app.main.blueprints.deputydev_cli.app.managers.authentication.authentication_manager import AuthenticationManager
+from app.main.blueprints.deputydev_cli.app.managers.authentication.authentication_manager import (
+    AuthenticationManager,
+)
 from app.main.blueprints.deputydev_cli.app.ui.screens.base_screen_handler import (
     BaseScreenHandler,
 )
@@ -25,9 +27,7 @@ class Authentication(BaseScreenHandler):
     async def render(self, **kwargs: Dict[str, Any]) -> Tuple[AppContext, ScreenType]:
         # Check if the current session is present and valid
         try:
-            authentication_manager = AuthenticationManager(
-                self.app_context.one_dev_client, self.session
-            )
+            authentication_manager = AuthenticationManager(self.app_context.one_dev_client, self.session)
             self.app_context.auth_token = await authentication_manager.authenticate_and_get_auth_token()
             return self.app_context, ScreenType.DEFAULT
         except Exception as e:
