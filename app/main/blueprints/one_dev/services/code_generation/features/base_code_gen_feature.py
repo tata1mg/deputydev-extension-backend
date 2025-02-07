ed80911afad51dc296bbfc0bc3e6f23a7f1b67af
Ankit Rana <ankitrana@FVFJCL6D1WFV.local>
2025-02-07 15:53:25 +0530

    CLI analytics changes LOC generated vs Accepted

--- app/main/blueprints/one_dev/services/code_generation/features/base_code_gen_feature.py
+++ app/main/blueprints/one_dev/services/code_generation/features/base_code_gen_feature.py
@@ -1,4 +1,5 @@
 import asyncio
+import re
 from abc import ABC, abstractmethod
 from typing import Any, Dict, Generic, List, TypeVar
 
--- app/main/blueprints/one_dev/services/code_generation/features/test_case_generator/main.py
+++ app/main/blueprints/one_dev/services/code_generation/features/test_case_generator/main.py
@@ -15,6 +15,7 @@ from app.main.blueprints.one_dev.services.code_generation.features.dataclass.mai
 from app.main.blueprints.one_dev.services.code_generation.features.test_case_generator.dataclasses.main import (
     TestCaseGenerationInput,
 )
+from app.main.blueprints.one_dev.services.code_generation.utils.utils import get_response_code_lines
 from app.main.blueprints.one_dev.services.repository.code_generation_job.main import (
     JobService,
 )
@@ -51,6 +52,7 @@ class TestCaseGenerationHandler(BaseCodeGenFeature[TestCaseGenerationInput]):
         )
 
         llm_response = await LLMHandler(prompt=prompt).get_llm_response_data(previous_responses=[])
+        code_lines = get_response_code_lines(llm_response.parsed_llm_data["response"])
         llm_meta.append(llm_response.llm_meta)
         await JobService.db_update(
             filters={"id": job_id},
@@ -59,6 +61,8 @@ class TestCaseGenerationHandler(BaseCodeGenFeature[TestCaseGenerationInput]):
                 "meta_info": {
                     "llm_meta": [meta.model_dump(mode="json") for meta in llm_meta],
                 },
+                "llm_model": llm_response.llm_meta.llm_model.value,
+                "loc": code_lines,
             },
         )
         await SessionChatService.db_create(
