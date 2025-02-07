d50f1a0f5f34e79c35625681ad916d4e26a63c33
Satyam Kumar <satyam.kumar1@1mg.com>
2025-02-07 13:23:44 +0530

    Fixes

--- app/main/blueprints/one_dev/services/code_generation/features/docs_generator/main.py
+++ app/main/blueprints/one_dev/services/code_generation/features/docs_generator/main.py
@@ -70,7 +70,7 @@ class DocsGenerationHandler(BaseCodeGenFeature[CodeDocsGenerationInput]):
         await SessionChatService.db_create(
             SessionChatDTO(
                 session_id=payload.session_id,
-                prompt_type=PromptFeatures.DIFF_CREATION,
+                prompt_type=PromptFeatures.DOCS_GENERATION,
                 llm_prompt=llm_response.raw_prompt,
                 llm_response=llm_response.raw_llm_response,
                 llm_model=llm_response.llm_meta.llm_model.value,
