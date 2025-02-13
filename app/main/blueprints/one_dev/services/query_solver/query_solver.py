fd1175dfd9b93887e14fab6753d4cb9b246148e1
Satyam Kumar <satyam.kumar1@1mg.com>
2025-02-13 10:58:33 +0530

    DD-393 - Streaming changes in route

--- app/main/blueprints/one_dev/services/query_solver/query_solver.py
+++ app/main/blueprints/one_dev/services/query_solver/query_solver.py
@@ -1,3 +1,16 @@
+from app.backend_common.services.llm.handler import LLMHandler
+from app.common.constants.constants import LLModels, PromptFeatures
+from app.common.services.prompt.factory import PromptFeatureFactory
+from app.main.blueprints.one_dev.services.query_solver.dataclasses.main import QuerySolverInput
+
+
 class QuerySolver:
-    def solve_query(self):
-        pass
+    async def solve_query(self, payload: QuerySolverInput):
+        prompt = PromptFeatureFactory.get_prompt(
+            prompt_feature=PromptFeatures.CODE_GENERATION,
+            model_name=LLModels.CLAUDE_3_POINT_5_SONNET,
+            init_params={"query": payload.query, "relevant_chunks": payload.relevant_chunks},
+        )
+
+        llm_response = await LLMHandler(prompt_handler=prompt, stream=True).get_llm_response_data(previous_responses=[])
+        return llm_response
