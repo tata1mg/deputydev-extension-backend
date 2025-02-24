0973af989e6b0f7f22be236aa85187c6940a9a73
Satyam Kumar <satyam.kumar1@1mg.com>
2025-02-24 10:46:44 +0530

    DD-393 - Add message_threads model

---
+++ app/backend_common/models/dao/postgres/message_threads.py
@@ -0,0 +1,51 @@
+from enum import Enum
+
+from tortoise import fields
+
+from .base import Base
+
+
+class MessageThread(Base):
+    serializable_keys = {
+        "id",
+        "session_id",
+        "actor",
+        "query_id",
+        "type",
+        "previous_query_ids",
+        "data",
+        "usage",
+        "llm_model",
+        "created_at",
+        "updated_at",
+    }
+
+    id = fields.IntField(primary_key=True)
+    session_id = fields.TextField()
+    actor = fields.TextField()
+    query_id = fields.TextField()
+    type = fields.TextField()
+    previous_query_ids = fields.JSONField()
+    data = fields.JSONField()
+    usage = fields.JSONField()
+    llm_model = fields.TextField()
+    created_at = fields.DatetimeField(auto_now_add=True)
+    updated_at = fields.DatetimeField(auto_now=True)
+
+
+    class Meta:
+        table = "message_threads"
+        indexes = (("session_id",),)
+
+    class Columns(Enum):
+        id = ("id",)
+        session_id = ("session_id",)
+        actor = ("actor",)
+        query_id = ("query_id",)
+        type = ("type",)
+        previous_query_ids = ("previous_query_ids",)
+        data = ("data",)
+        usage = ("usage",)
+        llm_model = ("llm_model",)
+        created_at = ("created_at",)
+        updated_at = ("updated_at",)
