be163638df6750464ed892d3097d2360b18d2b1d
Pratham verma <pratham.verma@1mg.com>
2025-02-26 12:03:07 +0530

    docstring added

--- app/main/blueprints/one_dev/services/serializer/past_sessions.py
+++ app/main/blueprints/one_dev/services/serializer/past_sessions.py
@@ -5,7 +5,23 @@ from app.backend_common.models.dto.message_sessions_dto import MessageSessionDat
 from app.main.blueprints.one_dev.services.serializer.base_serializers import BaseSerializer
 
 class PastSessionsSerializer(BaseSerializer):
+    """
+    Serializer for processing past message sessions data.
+
+    Inherits from BaseSerializer and implements the method to format raw message session data.
+    """
+
     def process_raw_data(self, raw_data: List[MessageSessionData], type: str) -> List[Dict[str, Any]]:
+        """
+        Processes raw message session data and formats it for output.
+
+        Args:
+            raw_data (List[MessageSessionData]): The raw message session data to be processed.
+            type (str): The type of data being serialized.
+
+        Returns:
+            List[Dict[str, Any]]: A list of formatted message session data.
+        """
         formatted_data = []
         current_time = datetime.now()
         for item in raw_data[::-1]:
@@ -19,6 +35,16 @@ class PastSessionsSerializer(BaseSerializer):
         return formatted_data
 
     def calculate_age(self, current_time: datetime, created_at: datetime) -> str:
+        """
+        Calculates the age of a message session based on the current time and its creation time.
+
+        Args:
+            current_time (datetime): The current time.
+            created_at (datetime): The creation time of the message session.
+
+        Returns:
+            str: A string representing the age in minutes, hours, or days.
+        """
         age_in_minutes = (current_time - created_at).total_seconds() / 60
         if age_in_minutes < 60:
             return f"{int(age_in_minutes)}m"
