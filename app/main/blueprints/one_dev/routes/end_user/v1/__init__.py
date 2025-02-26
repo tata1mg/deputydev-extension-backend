2794d5cce3f1659277617398666958162be62527
sachendra jain <sachendra.jain@1mg.com>
2025-02-26 19:32:32 +0530

    reranking route added

--- app/main/blueprints/one_dev/routes/end_user/v1/__init__.py
+++ app/main/blueprints/one_dev/routes/end_user/v1/__init__.py
@@ -4,6 +4,7 @@ from .auth import auth
 from .code_gen import code_gen
 from .config_handler import config
 from .repos import repos
+from .rerank import rerank
 
-blueprints = [code_gen, repos, auth, config]
+blueprints = [code_gen, repos, auth, config, rerank]
 one_dev_v1_bp = Blueprint.group(*blueprints, url_prefix="v1")
