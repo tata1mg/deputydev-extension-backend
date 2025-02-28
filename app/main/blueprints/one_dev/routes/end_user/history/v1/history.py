d552243a8ff21fbe20dab36509927f1d833d38f2
Pratham verma <pratham.verma@1mg.com>
2025-02-28 11:29:22 +0530

    getting session id from FE

--- app/main/blueprints/one_dev/routes/end_user/history/v1/history.py
+++ app/main/blueprints/one_dev/routes/end_user/history/v1/history.py
@@ -8,18 +8,18 @@ history = Blueprint("history", "/")
 
 @history.route("/chats", methods=["GET"])
 async def get_chats(_request: Request, **kwargs):
-    response = await PastWorkflows.get_past_chats()
+    headers = _request.headers
+    response = await PastWorkflows.get_past_chats(session_id=headers.get("X-Session-Id"))
     return send_response(response)
 
 @history.route("/sessions", methods=["GET"])
 async def get_sessions(_request: Request, **kwargs):
-    user_team_id = 1
+    user_team_id = 1 #TODO: need to change later
     response = await PastWorkflows.get_past_sessions(user_team_id)
     return send_response(response)
 
 @history.route("/delete_session", methods=["PUT"])
 async def delete_session(_request: Request, **kwargs):
     payload = _request.custom_json()
-    session_id = payload.get("sessionId")
-    response = await MessageSessionsRepository.soft_delete_message_session_by_id(session_id)
+    response = await MessageSessionsRepository.soft_delete_message_session_by_id(payload.get("sessionId"))
     return send_response(response)
