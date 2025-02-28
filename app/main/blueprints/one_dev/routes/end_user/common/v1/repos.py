8f00b1b530f386705b4914027dfdce06530ca76c
Satyam Kumar <satyam.kumar1@1mg.com>
2025-02-28 11:09:04 +0530

    DD-404 - Add ability to check both CLI and VSCODE_EXT versions correctly

--- app/main/blueprints/one_dev/routes/end_user/common/v1/repos.py
+++ app/main/blueprints/one_dev/routes/end_user/common/v1/repos.py
@@ -4,8 +4,8 @@ from torpedo import CONFIG, Request, send_response
 from app.main.blueprints.one_dev.services.repos.main import ReposHandler
 from app.main.blueprints.one_dev.utils.authenticate import authenticate
 from app.main.blueprints.one_dev.utils.dataclasses.main import AuthData
-from app.main.blueprints.one_dev.utils.pre_authenticate_handler import (
-    validate_cli_version,
+from app.main.blueprints.one_dev.utils.client.client_validator import (
+    validate_client_version,
 )
 
 repos = Blueprint("repos", "/")
@@ -13,7 +13,7 @@ repos = Blueprint("repos", "/")
 config = CONFIG.config
 
 
-@validate_cli_version
+@validate_client_version
 @repos.route("/get-registered-repo-details", methods=["GET"])
 @authenticate
 async def get_repos(_request: Request, auth_data: AuthData, **kwargs):
