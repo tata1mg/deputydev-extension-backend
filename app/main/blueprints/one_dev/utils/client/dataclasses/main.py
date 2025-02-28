a87ce9b326be62f8c86782427f9ffead483a087a
Satyam Kumar <satyam.kumar1@1mg.com>
2025-02-28 11:23:05 +0530

    DD-404 - Add ability to handle both CLI and VSCODE_EXT in configs

--- app/main/blueprints/one_dev/utils/client/dataclasses/main.py
+++ app/main/blueprints/one_dev/utils/client/dataclasses/main.py
@@ -1,7 +1,14 @@
 from enum import Enum
 
+from pydantic import BaseModel
+
 
 class Clients(Enum):
     CLI = "CLI"
     BACKEND = "BACKEND"
     VACODE_EXT = "VACODE_EXT"
+
+
+class ClientData(BaseModel):
+    client: Clients
+    client_version: str
