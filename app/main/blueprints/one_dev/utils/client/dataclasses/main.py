230da9b10cd57e876d64adfc120bab0f515a85e3
Satyam Kumar <satyam.kumar1@1mg.com>
2025-02-28 11:24:41 +0530

    DD-404 - Fix typo

--- app/main/blueprints/one_dev/utils/client/dataclasses/main.py
+++ app/main/blueprints/one_dev/utils/client/dataclasses/main.py
@@ -6,7 +6,7 @@ from pydantic import BaseModel
 class Clients(Enum):
     CLI = "CLI"
     BACKEND = "BACKEND"
-    VACODE_EXT = "VACODE_EXT"
+    VSCODE_EXT = "VSCODE_EXT"
 
 
 class ClientData(BaseModel):
