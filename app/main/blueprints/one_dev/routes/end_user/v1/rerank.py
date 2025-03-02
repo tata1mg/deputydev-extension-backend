07001b9f10f182bb1b5f79ae9b05ec8d27936535
sachendra jain <sachendra.jain@1mg.com>
2025-03-02 11:03:53 +0530

    reranking route

--- app/main/blueprints/one_dev/routes/end_user/v1/rerank.py
+++ app/main/blueprints/one_dev/routes/end_user/v1/rerank.py
@@ -4,25 +4,14 @@ from app.backend_common.services.chunking.reranker.handler.llm_based import LLMB
 from app.main.blueprints.one_dev.utils.authenticate import authenticate
 from torpedo import Request, send_response
 from app.main.blueprints.one_dev.utils.dataclasses.main import AuthData
-from pydantic import BaseModel
-from deputydev_core.services.chunking.chunk_info import ChunkInfo
-from typing import Optional
+from app.main.blueprints.one_dev.models.dto.reranking_input import RerankingInput
 
 rerank = Blueprint("rerank", "/")
 
 
-class RerankingInput(BaseModel):
-    query: str
-    relevant_chunks: list[ChunkInfo]
-    focus_chunks: Optional[list[ChunkInfo]]
-    is_llm_reranking_enabled: Optional[bool] = False
-
-
-# @rerank.route("/llm_based", methods=["POST"])
-# @authenticate
-# async def reranker(_request: Request, auth_data: AuthData, **kwargs):
-@rerank.route("/llm_reranking", methods=["POST"])
-async def reranker(_request: Request):
+@rerank.route("/llm_based", methods=["POST"])
+@authenticate
+async def reranker(_request: Request, auth_data: AuthData, **kwargs):
     payload = _request.custom_json()
     payload = RerankingInput(**payload)
     response = await LLMBasedChunkReranker().rerank(
