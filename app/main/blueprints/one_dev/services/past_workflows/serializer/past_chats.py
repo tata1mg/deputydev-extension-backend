be417fc693d6d56356eae8d8d93a2b2d4229ac1c
Pratham verma <pratham.verma@1mg.com>
2025-03-03 17:28:59 +0530

    parser comment resolved

--- app/main/blueprints/one_dev/services/past_workflows/serializer/past_chats.py
+++ app/main/blueprints/one_dev/services/past_workflows/serializer/past_chats.py
@@ -20,7 +20,7 @@ from app.main.blueprints.one_dev.services.query_solver.prompts.factory import (
 
 class PastChatsSerializer(BaseSerializer):
     def process_raw_data(self, raw_data: List[MessageThreadDTO], type: SerializerTypes) -> List[Dict[str, Any]]:
-
+        tool_use_map: Dict[str, Any] = {}
         formatted_data: List[Dict[str, Any]] = []
         for item in raw_data:
             message_type = item.message_type
@@ -33,20 +33,17 @@ class PastChatsSerializer(BaseSerializer):
                 formatted_data.append(
                     {"type": "TEXT_BLOCK", "content": {"text": item.query_vars["query"]}, "actor": actor}
                 )
+            elif message_type == MessageType("TOOL_RESPONSE"):
+                tool_use_id = response_block[0].content.tool_use_id
+                if tool_use_id not in tool_use_map:
+                    continue
+                tool_use_request_block = tool_use_map[tool_use_id]
+                tool_use_request_block["content"]["result_json"] = response_block[0].content.response
             else:
                 parser_class = PromptFeatureFactory.get_prompt(llm_model, PromptFeatures(prompt_feature))
-                parsed_blocks = parser_class.get_parsed_response_blocks(response_block)
+                parsed_blocks, tool_use_map = parser_class.get_parsed_response_blocks(response_block)
+                tool_use_map = tool_use_map
                 for block in parsed_blocks:
-                    if block["type"] == "TOOL_USE_RESPONSE_BLOCK":
-                        for request_block in formatted_data:
-                            if (
-                                request_block["type"] == "TOOL_USE_REQUEST_BLOCK"
-                                and request_block["content"]["tool_use_id"] == block["content"]["tool_use_id"]
-                            ):
-                                # Update the request block with data from the response block
-                                request_block["content"]["result_json"] = block["content"]["result_json"]
-                                break
-                        continue
                     block["actor"] = actor
                     formatted_data.append(block)
 
