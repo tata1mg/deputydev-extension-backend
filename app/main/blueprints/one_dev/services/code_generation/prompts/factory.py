6308c538166e5f55a73f50a244df07a6aafbe51a
Satyam Kumar <satyam.kumar1@1mg.com>
2025-03-07 12:42:16 +0530

    DD-408 - Add migrations for query summaries

--- app/main/blueprints/one_dev/services/code_generation/prompts/factory.py
+++ app/main/blueprints/one_dev/services/code_generation/prompts/factory.py
@@ -1,10 +1,11 @@
-from typing import Any, Dict, Type
+from typing import Dict, Type
 
 from app.backend_common.models.dto.message_thread_dto import LLModels
 from app.backend_common.services.llm.prompts.base_feature_prompt_factory import (
     BaseFeaturePromptFactory,
 )
 from app.backend_common.services.llm.prompts.base_prompt import BasePrompt
+from app.backend_common.services.llm.prompts.base_prompt_feature_factory import BasePromptFeatureFactory
 
 from .dataclasses.main import PromptFeatures
 from .feature_prompts.chat_filter.factory import ChatRankingPromptFactory
@@ -27,7 +28,7 @@ from .feature_prompts.test_plan_generation.factory import (
 )
 
 
-class PromptFeatureFactory:
+class PromptFeatureFactory(BasePromptFeatureFactory[PromptFeatures]):
     feature_prompt_factory_map: Dict[PromptFeatures, Type[BaseFeaturePromptFactory]] = {
         PromptFeatures.CODE_GENERATION: CodeGenerationPromptFactory,
         PromptFeatures.TEST_GENERATION: TestCaseGenerationPromptFactory,
@@ -42,12 +43,10 @@ class PromptFeatureFactory:
     }
 
     @classmethod
-    def get_prompt(
-        cls, prompt_feature: PromptFeatures, model_name: LLModels, init_params: Dict[str, Any]
-    ) -> BasePrompt:
-        feature_prompt_factory = cls.feature_prompt_factory_map.get(prompt_feature)
+    def get_prompt(cls, model_name: LLModels, feature: PromptFeatures) -> Type[BasePrompt]:
+        feature_prompt_factory = cls.feature_prompt_factory_map.get(feature)
         if not feature_prompt_factory:
-            raise ValueError(f"Invalid prompt feature: {prompt_feature}")
+            raise ValueError(f"Invalid prompt feature: {feature}")
 
         prompt_class = feature_prompt_factory.get_prompt(model_name)
-        return prompt_class(init_params)
+        return prompt_class
