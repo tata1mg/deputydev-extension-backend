5376962e66a0b3f1aea4ae1d979d1ad99b461bd0
Satyam Kumar <satyam.kumar1@1mg.com>
2025-03-07 12:42:16 +0530

    DD-408 - Add migrations for query summaries

--- app/main/blueprints/one_dev/models/dao/postgres/__init__.py
+++ app/main/blueprints/one_dev/models/dao/postgres/__init__.py
@@ -2,8 +2,10 @@ __all__ = [
     "SessionChats",
     "Job",
     "JobFeedback",
+    "QuerySummaries"
 ]
 
 from .job import Job
 from .job_feedback import JobFeedback
 from .session_chats import SessionChats
+from .query_summaries import QuerySummaries
