eebf79472e47728e66933f991d332ad65453dc60
sachendra jain <sachendra.jain@1mg.com>
2025-03-09 18:40:17 +0530

    rerank route updated

--- app/main/blueprints/one_dev/routes/end_user/common/v1/rerank.py
+++ app/main/blueprints/one_dev/routes/end_user/common/v1/rerank.py
@@ -1,17 +1,18 @@
 from sanic import Blueprint
 
-from app.backend_common.services.chunking.rerankers.handler.llm_based.reranker import LLMBasedChunkReranker
+from app.backend_common.services.chunking.reranker.handler.llm_based import LLMBasedChunkReranker
 from app.main.blueprints.one_dev.utils.authenticate import authenticate
 from torpedo import Request, send_response
 from app.main.blueprints.one_dev.utils.dataclasses.main import AuthData
 from app.main.blueprints.one_dev.models.dto.reranking_input import RerankingInput
 
-rerank = Blueprint("rerank", "/")
+rerank = Blueprint("rerank", "/rerank")
 
 
 @rerank.route("/llm_based", methods=["POST"])
-@authenticate
-async def reranker(_request: Request, auth_data: AuthData, **kwargs):
+# @authenticate
+# async def reranker(_request: Request, auth_data: AuthData, **kwargs):
+async def reranker(_request: Request):
     payload = _request.custom_json()
     payload = RerankingInput(**payload)
     response = await LLMBasedChunkReranker().rerank(
