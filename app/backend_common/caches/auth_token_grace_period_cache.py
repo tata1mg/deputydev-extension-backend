29d92b38cf58c496b5ff806426aea834051683d9
sachendra jain <sachendra.jain@1mg.com>
2025-03-18 18:53:02 +0530

    minor formatting

--- app/backend_common/caches/auth_token_grace_period_cache.py
+++ app/backend_common/caches/auth_token_grace_period_cache.py
@@ -3,4 +3,4 @@ from app.backend_common.caches.base import Base
 
 class AuthTokenGracePeriod(Base):
     _key_prefix = "auth_token_grace_period"
-    _expire_in_sec = 1800  # 1 week
+    _expire_in_sec = 1800  # 30 minutes
