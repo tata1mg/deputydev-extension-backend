e946c4bf90a1b136c803c958394652464cf58ca3
sachendra jain <sachendra.jain@1mg.com>
2025-03-18 17:59:13 +0530

    auth_token_grace_period

--- app/main/blueprints/one_dev/services/auth/auth.py
+++ app/main/blueprints/one_dev/services/auth/auth.py
@@ -5,12 +5,13 @@ from app.main.blueprints.one_dev.services.auth.login import Login
 
 class Auth:
     @classmethod
-    async def extract_and_verify_token(cls, headers: Dict[str, str]) -> Dict[str, Any]:
+    async def extract_and_verify_token(cls, headers: Dict[str, str], payload: Dict[str, Any]) -> Dict[str, Any]:
         authorization_header = headers.get("Authorization")
         if not authorization_header:
             raise Exception("Authorization header is missing")
 
         # decode encrypted session data and get the supabase access token
         encrypted_session_data = authorization_header.split(" ")[1]
-        result = await Login.verify_auth_token(encrypted_session_data)
+        enable_grace_period = payload.get("enable_grace_period") or False
+        result = await Login.verify_auth_token(encrypted_session_data, enable_grace_period=enable_grace_period)
         return result
