37cdae2fa3d00a5e1af4f6c21db2eb886c2d8e69
Satyam Kumar <satyam.kumar1@1mg.com>
2025-03-22 20:28:20 +0530

    DD-411 - Formatting

--- app/main/blueprints/deputy_dev/services/code_review/prompts/feature_prompts/comment_summarization/gpt_4o.py
+++ app/main/blueprints/deputy_dev/services/code_review/prompts/feature_prompts/comment_summarization/gpt_4o.py
@@ -1,7 +1,7 @@
 import json
 from typing import Any, AsyncIterator, Dict, List
 
-from app.backend_common.models.dto.message_thread_dto import TextBlockData, MessageData
+from app.backend_common.models.dto.message_thread_dto import MessageData, TextBlockData
 from app.backend_common.services.llm.dataclasses.main import (
     NonStreamingResponse,
     StreamingResponse,
--- app/main/blueprints/one_dev/routes/end_user/v1/websocket_connection/websocket_connection_blueprint.py
+++ app/main/blueprints/one_dev/routes/end_user/v1/websocket_connection/websocket_connection_blueprint.py
@@ -4,14 +4,16 @@ from typing import Any
 from sanic import Blueprint, response
 from torpedo import Request
 
-
-from app.backend_common.caches.websocket_connections_cache import WebsocketConnectionCache
+from app.backend_common.caches.websocket_connections_cache import (
+    WebsocketConnectionCache,
+)
 from app.main.blueprints.one_dev.utils.authenticate import authenticate
-from app.main.blueprints.one_dev.utils.client.client_validator import validate_client_version
+from app.main.blueprints.one_dev.utils.client.client_validator import (
+    validate_client_version,
+)
 from app.main.blueprints.one_dev.utils.client.dataclasses.main import ClientData
 from app.main.blueprints.one_dev.utils.dataclasses.main import AuthData
 from app.main.blueprints.one_dev.utils.session import ensure_session_id
-
 
 websocket_connection_v1_bp = Blueprint("websocket_connection_v1_bp", url_prefix="/websocket-connection")
 
@@ -29,7 +31,14 @@ async def connect(_request: Request, client_data: ClientData, auth_data: AuthDat
     print("Query Params: " + json.dumps(dict(_request.args)))
     connectionid = _request.headers.get("connectionid")
     print("Connected")
-    await WebsocketConnectionCache.set(key=connectionid, value={"session_id": session_id, "client_data": client_data.model_dump(mode="json"), "auth_data": auth_data.model_dump(mode="json")})
+    await WebsocketConnectionCache.set(
+        key=connectionid,
+        value={
+            "session_id": session_id,
+            "client_data": client_data.model_dump(mode="json"),
+            "auth_data": auth_data.model_dump(mode="json"),
+        },
+    )
     return response.json({"status": "SUCCESS"}, status=200)
 
 
