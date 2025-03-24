3b86e4d125ecd17ccc55efef98076b4084b6e7c1
Pratham verma <pratham.verma@1mg.com>
2025-03-24 07:08:10 +0530

    minor fi

--- app/main/blueprints/one_dev/routes/end_user/v1/ui_data/ui_data_blueprint.py
+++ app/main/blueprints/one_dev/routes/end_user/v1/ui_data/ui_data_blueprint.py
@@ -17,9 +17,9 @@ ui_data_v1_bp = Blueprint("ui_data_v1_bp", url_prefix="/ui_data")
 @ui_data_v1_bp.route("/profile_ui", methods=["GET"])
 @validate_client_version
 @authenticate
-async def get_ui_profile(_request: Request, **kwargs):
+async def get_ui_profile(_request: Request, auth_data: AuthData, **kwargs):
     try:
-        response = await UIProfile.get_ui_profile(user_team_id=1)
+        response = await UIProfile.get_ui_profile(user_team_id=auth_data.user_team_id)
     except Exception as e:
         raise BadRequestException(f"Failed to fetch ui profile data: {str(e)}")
     return send_response(response, headers=kwargs.get("response_headers"))
