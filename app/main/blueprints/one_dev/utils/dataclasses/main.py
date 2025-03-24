177189c6aa814ecbad298a28fe833a2b93c74522
Satyam Kumar <satyam.kumar1@1mg.com>
2025-03-24 08:24:31 +0530

    DD-411 - Add some extra data in stream

--- app/main/blueprints/one_dev/utils/dataclasses/main.py
+++ app/main/blueprints/one_dev/utils/dataclasses/main.py
@@ -1,5 +1,7 @@
+from typing import Optional, Tuple
 from pydantic import BaseModel
 
 
 class AuthData(BaseModel):
     user_team_id: int
+    refresh_session_data: Optional[Tuple[str, str, str]] = None
