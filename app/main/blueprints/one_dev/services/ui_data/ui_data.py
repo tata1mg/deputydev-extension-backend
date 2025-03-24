3e8f38f40b5408a9f3fbcc98bf691254a9fddf6c
Satyam Kumar <satyam.kumar1@1mg.com>
2025-03-24 13:56:21 +0530

    DD-413 - Add client download link in clients validation

--- app/main/blueprints/one_dev/services/ui_data/ui_data.py
+++ app/main/blueprints/one_dev/services/ui_data/ui_data.py
@@ -1,27 +1,33 @@
-from typing import Any, Dict
+from typing import Any, Dict, List
 
 from torpedo.exceptions import BadRequestException
 
 from app.backend_common.models.dto.message_thread_dto import MessageCallChainCategory
-from app.backend_common.repository.message_sessions.repository import MessageSessionsRepository
-from app.backend_common.repository.message_threads.repository import MessageThreadsRepository
+from app.backend_common.repository.message_sessions.repository import (
+    MessageSessionsRepository,
+)
+from app.backend_common.repository.message_threads.repository import (
+    MessageThreadsRepository,
+)
 
 
 class UIProfile:
     @classmethod
     async def get_ui_profile(cls, user_team_id: int) -> Dict[str, Any]:
         try:
-            sessions_ids = await MessageSessionsRepository.get_message_sessions_ids(user_team_id=user_team_id, session_type = "CODE_GENERATION_V2")
-            total_queries = await MessageThreadsRepository.get_total_user_queries(session_ids=sessions_ids, call_chain_category=MessageCallChainCategory.CLIENT_CHAIN)
+            sessions_ids = await MessageSessionsRepository.get_message_sessions_ids(
+                user_team_id=user_team_id, session_type="CODE_GENERATION_V2"
+            )
+            total_queries = await MessageThreadsRepository.get_total_user_queries(
+                session_ids=sessions_ids, call_chain_category=MessageCallChainCategory.CLIENT_CHAIN
+            )
             ui_profile_data = await cls.get_ui_profile_data(total_queries)
-            return {
-                "ui_profile_data": ui_profile_data
-            }
+            return {"ui_profile_data": ui_profile_data}
         except Exception as e:
             raise BadRequestException(f"Failed to fetch ui profile data: {str(e)}")
 
     @classmethod
-    async def get_ui_profile_data(cls, total_queries: int) -> Dict[str, Any]:
+    async def get_ui_profile_data(cls, total_queries: int) -> List[Dict[str, Any]]:
         return [
             {
                 "label": "Usage",
@@ -36,24 +42,24 @@ class UIProfile:
                         <div className="text-sm text-gray-400 text-right">{total_queries} of Unlimited</div>
                     </div>
                     </div>
-                </div>"""
+                </div>""",
             },
             {
                 "label": "Feature Request",
                 "type": "Hyperlink",
                 "icon": "https://onemg.gumlet.io/dd_request_feature_24_03.png",
-                "url": "https://forms.gle/Abd1FJJVf3J2daLP7"
+                "url": "https://forms.gle/Abd1FJJVf3J2daLP7",
             },
             {
                 "label": "Report a Bug",
                 "type": "Hyperlink",
                 "icon": "https://onemg.gumlet.io/dd_report_bug_24_03.png",
-                "url": "https://forms.gle/s2Youjzo63YU9k7s9"
+                "url": "https://forms.gle/s2Youjzo63YU9k7s9",
             },
             {
                 "label": "r/DeputyDev",
                 "type": "Hyperlink",
                 "icon": "https://onemg.gumlet.io/dd_reddit_logo_v2_23_03.png",
-                "url": "https://www.reddit.com/r/DeputyDev/"
-            }
+                "url": "https://www.reddit.com/r/DeputyDev/",
+            },
         ]
--- app/main/blueprints/one_dev/utils/client/client_validator.py
+++ app/main/blueprints/one_dev/utils/client/client_validator.py
@@ -1,6 +1,7 @@
 from functools import wraps
 from typing import Any, Optional, Tuple
 
+from deputydev_core.utils.config_manager import ConfigManager
 from deputydev_core.utils.constants.enums import Clients
 from deputydev_core.utils.constants.error_codes import APIErrorCodes
 from torpedo import Request
@@ -15,9 +16,14 @@ from app.main.blueprints.one_dev.utils.client.dataclasses.main import ClientData
 from app.main.blueprints.one_dev.utils.version import compare_version
 
 
-def validate_version(client: Clients, client_version: str) -> Tuple[bool, Optional[str]]:
+def validate_version(client: Clients, client_version: str) -> Tuple[bool, Optional[str], Optional[str]]:
+    """
+    Validate the client version and return a boolean indicating if the version is supported
+    Optionally return the minimum version supported and the download link for the client
+    """
     is_valid = True
     min_version_supported = None
+    download_link = None
     if client == Clients.CLI:
         is_valid = compare_version(client_version, MIN_SUPPORTED_CLI_VERSION, ">=")
         if not is_valid:
@@ -30,8 +36,9 @@ def validate_version(client: Clients, client_version: str) -> Tuple[bool, Option
         is_valid = compare_version(client_version, MIN_SUPPORTED_VSCODE_EXT_VERSION, ">=")
         if not is_valid:
             min_version_supported = MIN_SUPPORTED_VSCODE_EXT_VERSION
+            download_link = ConfigManager.configs["CLIENT_DOWNLOAD_LINKS"].get(client.value)
 
-    return is_valid, min_version_supported
+    return is_valid, min_version_supported, download_link
 
 
 def validate_client_version(func: Any):
@@ -60,12 +67,18 @@ def validate_client_version(func: Any):
                 meta={"error_code": APIErrorCodes.CLIENT_VERSION_HEADER_MISSING.value},
             )
 
-        is_valid, upgrade_version = validate_version(client=validated_client, client_version=client_version)
+        is_valid, upgrade_version, client_download_link = validate_version(
+            client=validated_client, client_version=client_version
+        )
 
         if not is_valid:
             raise BadRequestException(
                 error=upgrade_version,
-                meta={"error_code": APIErrorCodes.INVALID_CLIENT_VERSION.value, "upgrade_version": upgrade_version},
+                meta={
+                    "error_code": APIErrorCodes.INVALID_CLIENT_VERSION.value,
+                    "upgrade_version": upgrade_version,
+                    **({} if client_download_link is None else {"client_download_link": client_download_link}),
+                },
             )
 
         return await func(
