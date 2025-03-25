b4deb4a8ceb09cc085fe45549421e21987735d94
Ankit Rana <ankit.rana1@1mg.com>
2025-03-25 19:07:11 +0530

    Review changes

---
+++ app/backend_common/repository/pixel_events/repository.py
@@ -0,0 +1,16 @@
+import pytz
+
+from app.backend_common.models.dao.postgres.pixel_events import PixelEvents
+from app.backend_common.models.dto.pixel_events_dto import (
+    PixelEventsData,
+    PixelEventsDTO,
+)
+from app.backend_common.repository.db import DB
+
+
+class PixelEventsRepository:
+    @classmethod
+    async def db_insert(cls, pixel_events_dto: PixelEventsData) -> PixelEventsDTO:
+        payload = pixel_events_dto.model_dump()
+        row = await DB.insert_row(PixelEvents, payload)
+        return row
