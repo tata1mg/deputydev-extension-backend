cd24050ce793f1c7aabfc8adaac3dc60e9203ead
Satyam Kumar <satyam.kumar1@1mg.com>
2025-03-26 13:10:36 +0530

    DD-417 - Diff applicator fixes

--- app/service.py
+++ app/service.py
@@ -13,7 +13,7 @@ from app.main.blueprints.one_dev.routes.end_user import (  # noqa : E402
     one_dev_end_user_bp,
 )
 
-main_app_bp = Blueprint.group(jiva_end_user_bp, deputy_dev_end_user_bp, one_dev_end_user_bp, url_prefix="/")
+main_app_bp = Blueprint.group(deputy_dev_end_user_bp, one_dev_end_user_bp, url_prefix="/")
 
 
 if __name__ == "__main__":
