4f65daf353e75a13fe535a922fd9839c25c072ce
Ankit Rana <ankit.rana1@1mg.com>
2025-03-26 16:33:11 +0530

    review changes

--- app/backend_common/models/dto/pixel_events_dto.py
+++ app/backend_common/models/dto/pixel_events_dto.py
@@ -4,12 +4,13 @@ from typing import List, Optional
 from pydantic import BaseModel
 
 from app.main.blueprints.one_dev.utils.client.dataclasses.main import Clients
+from app.backend_common.constants.constants import PixelEvents
 
 
 class PixelEventsData(BaseModel):
     event_id: str
     session_id: int
-    event_type: str
+    event_type: PixelEvents
     lines: int
     file_path: Optional[str] = None
     client_version: str
