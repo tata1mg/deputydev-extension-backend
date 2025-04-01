a3530d73f8cd356d66c18c640065923d56bb8d0e
Satyam Kumar <satyam.kumar1@1mg.com>
2025-04-01 15:12:02 +0530

    DD-425 - Add ability to tool use in inline edit.

--- app/main/blueprints/one_dev/services/query_solver/inline_editor.py
+++ app/main/blueprints/one_dev/services/query_solver/inline_editor.py
@@ -1,7 +1,7 @@
 import asyncio
 from typing import Any, Dict
 
-from app.backend_common.models.dto.message_thread_dto import LLModels
+from app.backend_common.models.dto.message_thread_dto import LLModels, ToolUseResponseContent, ToolUseResponseData
 from app.backend_common.services.llm.handler import LLMHandler
 from app.main.blueprints.one_dev.models.dto.job import JobDTO
 from app.main.blueprints.one_dev.services.query_solver.dataclasses.main import (
@@ -10,6 +10,7 @@ from app.main.blueprints.one_dev.services.query_solver.dataclasses.main import (
 from app.main.blueprints.one_dev.services.query_solver.prompts.dataclasses.main import (
     PromptFeatures,
 )
+from app.main.blueprints.one_dev.services.query_solver.tools.related_code_searcher import RELATED_CODE_SEARCHER
 from app.main.blueprints.one_dev.services.repository.code_generation_job.main import (
     JobService,
 )
@@ -19,22 +20,43 @@ from .prompts.factory import PromptFeatureFactory
 
 class InlineEditGenerator:
     async def get_inline_edit_diff_suggestion(self, payload: InlineEditInput) -> Dict[str, Any]:
+        if not (payload.query and payload.code_selection) and not payload.tool_use_response:
+            raise ValueError("Either query and code selection or tool use response must be provided")
+
         llm_handler = LLMHandler(prompt_factory=PromptFeatureFactory, prompt_features=PromptFeatures)
-        llm_response = await llm_handler.start_llm_query(
-            prompt_feature=PromptFeatures.INLINE_EDITOR,
-            llm_model=LLModels.CLAUDE_3_POINT_5_SONNET,
-            prompt_vars={
-                "query": payload.query,
-                "relevant_chunks": payload.relevant_chunks,
-                "code_selection": payload.code_selection,
-                "deputy_dev_rules": payload.deputy_dev_rules,
-            },
-            previous_responses=[],
-            tools=[],
-            stream=False,
-            session_id=payload.session_id,
-        )
-        return llm_response.parsed_content[0]
+        tools_to_use = [RELATED_CODE_SEARCHER]
+        
+        if payload.query and payload.code_selection:
+            llm_response = await llm_handler.start_llm_query(
+                prompt_feature=PromptFeatures.INLINE_EDITOR,
+                llm_model=LLModels.CLAUDE_3_POINT_5_SONNET,
+                prompt_vars={
+                    "query": payload.query,
+                    "relevant_chunks": payload.relevant_chunks,
+                    "code_selection": payload.code_selection,
+                    "deputy_dev_rules": payload.deputy_dev_rules,
+                },
+                previous_responses=[],
+                tools=tools_to_use,
+                stream=False,
+                session_id=payload.session_id,
+            )
+            return llm_response.parsed_content[0]
+
+        else:
+            llm_response = await llm_handler.submit_tool_use_response(
+                session_id=payload.session_id,
+                tool_use_response=ToolUseResponseData(
+                    content=ToolUseResponseContent(
+                        tool_name=payload.tool_use_response.tool_name,
+                        tool_use_id=payload.tool_use_response.tool_use_id,
+                        response=tool_response,
+                    )
+                ),
+                tools=tools_to_use,
+                stream=True,
+            )
+            return llm_response.parsed_content[0]
 
     async def start_job(self, payload: InlineEditInput, job_id: int) -> int:
         try:
