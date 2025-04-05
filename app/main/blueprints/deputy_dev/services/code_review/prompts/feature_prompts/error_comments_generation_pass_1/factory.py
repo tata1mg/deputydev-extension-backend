34147a0f31df8d3b12224b01efab5a629748b338
Ankit Rana <ankit.rana1@1mg.com>
2025-04-05 14:40:35 +0530

    Move PR review from 3.5 sonnet to 3.7 sonnet

--- app/main/blueprints/deputy_dev/services/code_review/prompts/feature_prompts/error_comments_generation_pass_1/factory.py
+++ app/main/blueprints/deputy_dev/services/code_review/prompts/feature_prompts/error_comments_generation_pass_1/factory.py
@@ -7,10 +7,14 @@ from app.backend_common.services.llm.prompts.base_feature_prompt_factory import 
 from app.backend_common.services.llm.prompts.base_prompt import BasePrompt
 
 from .claude_3_point_5_sonnet import Claude3Point5ErrorCommentsGenerationPass1Prompt
+from .claude_3_point_7_sonnet import Claude3Point7ErrorCommentsGenerationPass1Prompt
 
 
 class ErrorCommentsGenerationPass1PromptFactory(BaseFeaturePromptFactory):
-    prompts = {LLModels.CLAUDE_3_POINT_5_SONNET: Claude3Point5ErrorCommentsGenerationPass1Prompt}
+    prompts = {
+        LLModels.CLAUDE_3_POINT_5_SONNET: Claude3Point5ErrorCommentsGenerationPass1Prompt,
+        LLModels.CLAUDE_3_POINT_7_SONNET: Claude3Point7ErrorCommentsGenerationPass1Prompt,
+    }
 
     @classmethod
     def get_prompt(cls, model_name: LLModels) -> Type[BasePrompt]:
