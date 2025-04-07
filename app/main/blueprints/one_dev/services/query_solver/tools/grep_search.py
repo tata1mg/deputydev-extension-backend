6adf8c2c5af95fdea9ffd82366c23b638bbe96c5
Pratham verma <pratham.verma@1mg.com>
2025-04-07 16:19:51 +0530

    minor change

--- app/main/blueprints/one_dev/services/query_solver/tools/grep_search.py
+++ app/main/blueprints/one_dev/services/query_solver/tools/grep_search.py
@@ -3,17 +3,23 @@ from app.backend_common.services.llm.dataclasses.main import ConversationTool
 GREP_SEARCH = ConversationTool(
     name="grep_search",
     description="""
-        Searches for files in a given directory path.
-        It is very important to provide the correct and complete directory path to search in, otherwise the tool will return the results from the repo root.
-        This can also be used to list down files in a given directory.
-        This gives at max 100 files.
-    """,
+    Searches for the content of all files in a given directory path.
+    It is very important to provide the correct and complete directory path to search in; otherwise, the tool will return results from the repo root.
+    This tool reads the contents of the files and can return up to 100 files.
+""",
     input_schema={
         "type": "object",
         "properties": {
             "directory": {
                 "type": "string",
                 "description": "The complete path of the directory to search in",
+            },
+            "search_terms": {
+                "type": "array",
+                "items": {
+                    "type": "string"
+                },
+                "description": "List of search terms to match against file names.",
             },
         },
         "required": ["directory"],
