da6e78a20f8a89f1302780a0c8b0ecfa42cfd5b2
Satyam Kumar <satyam.kumar1@1mg.com>
2025-04-09 15:26:27 +0530

    DD-428 - Fix grep search

--- app/main/blueprints/one_dev/services/query_solver/tools/grep_search.py
+++ app/main/blueprints/one_dev/services/query_solver/tools/grep_search.py
@@ -3,10 +3,11 @@ from app.backend_common.services.llm.dataclasses.main import ConversationTool
 GREP_SEARCH = ConversationTool(
     name="grep_search",
     description="""
-    Searches for the content of all files in a given directory path.
-    It is very important to provide the correct and complete directory path to search in; otherwise, the tool will return results from the repo root.
+    This tool searches the content in all the files in a given directory path for specified search terms. It uses grep to perform the search.
+    It does pattern matching on the file contents recursively. This can be used to search for specific patterns/words/sentences/keywords on the files in a directory in a very quick and efficient way.
     This tool reads the contents of the files and returns the lines that match the search terms.
-""",
+    This should be used for queries that require to find specific pattern in the files, or which require a find and replace operation.
+    """,
     input_schema={
         "type": "object",
         "properties": {
@@ -16,9 +17,7 @@ GREP_SEARCH = ConversationTool(
             },
             "search_terms": {
                 "type": "array",
-                "items": {
-                    "type": "string"
-                },
+                "items": {"type": "string"},
                 "description": "List of search terms to match against file names.",
             },
         },
