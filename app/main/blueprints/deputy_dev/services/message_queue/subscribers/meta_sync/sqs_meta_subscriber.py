107a1ab5e605a533f85b7c4c01eba5adb1ff5219
sachendra jain <sachendra.jain@1mg.com>
2025-04-10 01:15:06 +0530

    logger added

--- app/main/blueprints/deputy_dev/services/message_queue/subscribers/meta_sync/sqs_meta_subscriber.py
+++ app/main/blueprints/deputy_dev/services/message_queue/subscribers/meta_sync/sqs_meta_subscriber.py
@@ -4,10 +4,11 @@ from app.main.blueprints.deputy_dev.services.message_queue.subscribers.base.sqs_
 from app.main.blueprints.deputy_dev.services.stats_collection.stats_collection_factory import (
     StatsCollectionFactory,
 )
-
+from sanic.log import logger
 
 class SQSMetaSubscriber(SQSSubscriber):
     def get_queue_name(self):
+        logger.info(f"METASYNC queue getting picked: {self.config.get('SQS')}")
         return self.config.get("SQS", {}).get("SUBSCRIBE", {}).get("METASYNC", {}).get("QUEUE_NAME", "")
 
     def get_queue_config(self):
