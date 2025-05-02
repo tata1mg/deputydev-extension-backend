d89545de3fd997d8ca1bb07193ba016372252a87
Pratham verma <pratham.verma@1mg.com>
2025-05-02 19:02:10 +0530

    minor fixed

--- app/main/blueprints/one_dev/routes/end_user/v1/extension_settings/extension_settings.py
+++ app/main/blueprints/one_dev/routes/end_user/v1/extension_settings/extension_settings.py
@@ -2,7 +2,7 @@ from typing import Any
 
 from sanic import Blueprint
 from torpedo import Request, send_response
-from torpedo.exceptions import BadRequestException
+from torpedo.exceptions import BadRequestException, HTTPRequestException
 from app.main.blueprints.one_dev.models.dto.extension_settings_dto import ExtensionSettingsData, Settings
 from app.main.blueprints.one_dev.services.repository.extension_settings.repository import ExtensionSettingsRepository
 from app.main.blueprints.one_dev.utils.authenticate import authenticate
@@ -33,7 +33,7 @@ async def update_extension_settings(_request: Request, client_data: ClientData, 
             )
         )
     except Exception as e:
-        raise Exception(f"Failed to update extension settings: {str(e)}")
+        raise HTTPRequestException(f"Failed to update extension settings: {str(e)}")
     return send_response({"message": "Extension settings updated successfully"}, headers=kwargs.get("response_headers"))
 
 @extension_settings_v1_bp.route("/get_extension_settings", methods=["GET"])
@@ -46,5 +46,5 @@ async def get_extension_settings(_request: Request, auth_data: AuthData, **kwarg
             raise BadRequestException("Extension settings not found")
         settings = response.settings
     except Exception as e:
-        raise Exception(f"Failed to get extension settings: {str(e)}")
+        raise HTTPRequestException(f"Failed to get extension settings: {str(e)}")
     return send_response(settings.model_dump(mode="json"), headers=kwargs.get("response_headers"))
