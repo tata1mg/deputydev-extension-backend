43b9ff491772fe555e5afe3c5c7c7dc6a2d377ce
Ankit Rana <ankit.rana1@1mg.com>
2025-05-08 20:14:54 +0530

    Mocked embedding clinet to Extension

--- app/main/blueprints/deputy_dev/client/one_dev_review_client.py
+++ app/main/blueprints/deputy_dev/client/one_dev_review_client.py
@@ -29,7 +29,7 @@ class OneDevReviewClient(BaseHTTPClient):
     async def create_embedding(self, payload: Dict[str, Any], headers: Dict[str, str]) -> Dict[str, Any]:
         path = f"{self.get_embedding_base_url()}/end_user/v1/code-gen/create-embedding"
         result = await self.post(
-            url=path, json=payload, headers={**headers, "X-Client-Version": "1.0.0", "X-Client": "REVIEW"}
+            url=path, json=payload, headers={**headers, "X-Client-Version": "2.0.1", "X-Client": "VSCODE_EXT"}
         )
         return (await result.json()).get("data")
 
