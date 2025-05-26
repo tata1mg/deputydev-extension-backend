7042d2dfdd9b382163c178f1a680e1dc4543f4d5
sachendra jain <sachendra.jain@1mg.com>
2025-05-26 12:24:19 +0530

    latency fix

---
+++ app/main/blueprints/one_dev/services/query_solver/prompts/feature_prompts/inline_editor/claude_4_sonnet.py
@@ -0,0 +1,5 @@
+from app.main.blueprints.one_dev.services.query_solver.prompts.feature_prompts.inline_editor.claude_3_point_5_sonnet import Claude3Point5InlineEditorPrompt
+
+
+class Claude4InlineEditorPrompt(Claude3Point5InlineEditorPrompt):
+    pass
