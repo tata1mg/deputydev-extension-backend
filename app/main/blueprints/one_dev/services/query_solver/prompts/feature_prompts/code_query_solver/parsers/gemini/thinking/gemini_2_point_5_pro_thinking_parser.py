5a7a09840ffa7ed1d5e9e439f6187283e9ab2b72
sachendra jain <sachendra.jain@1mg.com>
2025-05-27 15:06:56 +0530

    flash 2.5 integration

---
+++ app/main/blueprints/one_dev/services/query_solver/prompts/feature_prompts/code_query_solver/parsers/gemini/thinking/gemini_2_point_5_pro_thinking_parser.py
@@ -0,0 +1,7 @@
+from app.main.blueprints.one_dev.services.query_solver.prompts.feature_prompts.code_query_solver.parsers.gemini.thinking.base_thinking_parser import (
+    ThinkingParser,
+)
+
+
+class Gemini2Point5ProThinkingParser(ThinkingParser):
+    pass
