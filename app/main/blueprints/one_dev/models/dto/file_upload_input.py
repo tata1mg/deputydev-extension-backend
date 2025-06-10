d9e5c58ee49ab051f4a22d01f9bfd6e266bf3061
Vaibhav Meena <vaibhav.meena@1mg.com>
2025-06-10 20:31:23 +0530

    add id on analytics events

--- app/main/blueprints/one_dev/models/dto/file_upload_input.py
+++ app/main/blueprints/one_dev/models/dto/file_upload_input.py
@@ -1,11 +1,12 @@
-from typing import Literal
-from pydantic import BaseModel, conint
+from typing import Literal, Annotated
+from pydantic import BaseModel, Field
 
 
 class FileUploadPostInput(BaseModel):
     file_name: str
-    file_size: conint(ge=1, le=5 * 1024 * 1024 * 1024)
-    file_type: Literal["image/jpeg", "image/jpg", "image/png", "image/webp"]
+    file_size: Annotated[int, Field(ge=1, le=5 * 1024 * 1024 * 1024)]
+    file_type: Literal["image/jpeg", "image/jpg", "image/png", "image/webp", "application/json"]
+    folder : Literal["image", "payload"] = "image"
 
 
 class FileUploadGetInput(BaseModel):
