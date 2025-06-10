611d04728a1ea2b7ad598adf82ccb6487a891781
Vaibhav Meena <vaibhav.meena@1mg.com>
2025-06-10 20:31:23 +0530

    add id on analytics events

--- app/main/blueprints/one_dev/services/config/config_fetcher.py
+++ app/main/blueprints/one_dev/services/config/config_fetcher.py
@@ -142,9 +142,14 @@ class ConfigFetcher:
             },
             "VSCODE_IGNORE_FILES": {"EXCLUDE_DIRS": [], "EXCLUDE_EXTS": []},
             "VSCODE_LOGS_RETENTION_DAYS": 7,
+            # TODO: remove after 7.0.0 force upgrade
             "CHAT_IMAGE_UPLOAD": {
-                "MAX_BYTES": ConfigManager.configs["CHAT_IMAGE_UPLOAD"]["MAX_BYTES"],
-                "SUPPORTED_MIMETYPES": ConfigManager.configs["CHAT_IMAGE_UPLOAD"]["SUPPORTED_MIMETYPES"],
+                "MAX_BYTES": ConfigManager.configs["CHAT_FILE_UPLOAD"]["MAX_BYTES"],
+                "SUPPORTED_MIMETYPES": ConfigManager.configs["CHAT_FILE_UPLOAD"]["SUPPORTED_MIMETYPES"],
+            },
+            "CHAT_FILE_UPLOAD": {
+                "MAX_BYTES": ConfigManager.configs["CHAT_FILE_UPLOAD"]["MAX_BYTES"],
+                "SUPPORTED_MIMETYPES": ConfigManager.configs["CHAT_FILE_UPLOAD"]["SUPPORTED_MIMETYPES"],
             },
         },
     }
