e9af755921d33796c161c06924dd5927895a7a32
Satyam Kumar <satyam.kumar1@1mg.com>
2025-06-13 17:55:55 +0530

    Fix issue with tool resp failure

--- app/backend_common/services/llm/providers/anthropic/llm_provider.py
+++ app/backend_common/services/llm/providers/anthropic/llm_provider.py
@@ -188,7 +188,9 @@ class Anthropic(BaseLLMProvider):
         )
 
         # remove last block from the messages if not tool response and last block is tool use request
-        if not tool_use_response and (messages and messages[-1].role == ConversationRole.ASSISTANT and messages[-1].content):
+        if not tool_use_response and (
+            messages and messages[-1].role == ConversationRole.ASSISTANT and messages[-1].content
+        ):
             last_content = messages[-1].content[-1]
             if isinstance(last_content, dict) and last_content.get("type") == "tool_use":
                 # remove the tool use request if the user has not responded to it
@@ -249,7 +251,6 @@ class Anthropic(BaseLLMProvider):
             "messages": [message.model_dump(mode="json") for message in messages],
             "tools": [tool.model_dump(mode="json", by_alias=True, exclude_defaults=True) for tool in tools],
         }
-
 
         if model_config.get("THINKING") and model_config["THINKING"]["ENABLED"]:
             llm_payload["thinking"] = {"type": "enabled", "budget_tokens": model_config["THINKING"]["BUDGET_TOKENS"]}
