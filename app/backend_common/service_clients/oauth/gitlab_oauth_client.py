8e74eb45a3e3baf6f014870ca09b36cc9f6db89c
Satyam Kumar <satyam.kumar1@1mg.com>
2025-06-14 09:39:14 +0530

    Remove all commented out code

--- app/backend_common/service_clients/oauth/gitlab_oauth_client.py
+++ app/backend_common/service_clients/oauth/gitlab_oauth_client.py
@@ -41,15 +41,6 @@ class GitlabOAuthClient:
 
         # parse for token
 
-        # e.g. resp
-        # {
-        #     "access_token": "de6780bc506a0446309bd9362820ba8aed28aa506c71eedbe1c5c4f9dd350e54",
-        #     "token_type": "bearer",
-        #     "expires_in": 7200,
-        #     "refresh_token": "8257e65c97202ed1726cf9571600918f3bffb2544b26e00a61df9897668c33a1",
-        #     "created_at": 1607635748,
-        # }
-
     @classmethod
     async def refresh_access_token(cls, refresh_token: str) -> str:
         """Get new access token using refresh token."""
@@ -67,12 +58,3 @@ class GitlabOAuthClient:
         async with session.post(url=url, json=data) as response:
             response.raise_for_status()
             return await response.json()
-
-        # e.g. resp
-        # {
-        #     "access_token": "c97d1fe52119f38c7f67f0a14db68d60caa35ddc86fd12401718b649dcfa9c68",
-        #     "token_type": "bearer",
-        #     "expires_in": 7200,
-        #     "refresh_token": "803c1fd487fec35562c205dac93e9d8e08f9d3652a24079d704df3039df1158f",
-        #     "created_at": 1628711391,
-        # }
