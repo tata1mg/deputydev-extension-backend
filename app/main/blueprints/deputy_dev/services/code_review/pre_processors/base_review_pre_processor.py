762523877af93382b608ef7f42d80ffacfcb2194
Your Name <ankit.rana1@1mg.com>
2025-06-26 11:38:29 +0530

    Initial commit

---
+++ app/main/blueprints/deputy_dev/services/code_review/pre_processors/base_review_pre_processor.py
@@ -0,0 +1,13 @@
+from abc import ABC, abstractmethod
+
+class BaseReviewPreProcessor(ABC):
+    def __init__(self):
+        self.is_valid = True
+        self.review_status = None
+
+    @abstractmethod
+    async def pre_process_pr(self, data: dict):
+        """
+        Abstract method to pre-process a review request.
+        """
+        pass
