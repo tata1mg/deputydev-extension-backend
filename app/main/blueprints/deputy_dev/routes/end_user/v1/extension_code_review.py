e1dc6683dbf7a305b974ec39eb552e72b3c3d5cb
sachendra jain <sachendra.jain@1mg.com>
2025-07-03 01:11:58 +0530

    testion

--- app/main/blueprints/deputy_dev/routes/end_user/v1/extension_code_review.py
+++ app/main/blueprints/deputy_dev/routes/end_user/v1/extension_code_review.py
@@ -32,6 +32,8 @@ async def code_review_history(_request: Request, auth_data: AuthData, **kwargs):
     try:
         # Extract query parameters
         query_params = _request.request_params()
+
+
         review_history_params = ReviewHistoryParams(**query_params, user_team_id=auth_data.user_team_id)
 
         # Initialize manager and fetch reviews
