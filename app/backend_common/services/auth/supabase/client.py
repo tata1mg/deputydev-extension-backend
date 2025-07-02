f1fd519a0dcd4cb03e2b851d4c4c3a7a54cd34be
sachendra jain <sachendra.jain@1mg.com>
2025-07-02 14:04:43 +0530

    pipeline fix

--- app/backend_common/services/auth/supabase/client.py
+++ app/backend_common/services/auth/supabase/client.py
@@ -1,14 +1,22 @@
+from typing import TYPE_CHECKING
+
 from torpedo import CONFIG
 
 from supabase import Client, create_client
 from supabase.lib.client_options import SyncClientOptions
+
+if TYPE_CHECKING:
+    from supabase._sync.client import SyncClient
+
 
 class SupabaseClient:
     _instance: Client = None
     supabase = CONFIG.config["SUPABASE"]
 
     @classmethod
-    def get_instance(cls):
+    def get_instance(cls) -> "SyncClient":
         if cls._instance is None:
-            cls._instance = create_client(cls.supabase["URL"], cls.supabase["KEY"], options=SyncClientOptions(auto_refresh_token=False))
+            cls._instance = create_client(
+                cls.supabase["URL"], cls.supabase["KEY"], options=SyncClientOptions(auto_refresh_token=False)
+            )
         return cls._instance
