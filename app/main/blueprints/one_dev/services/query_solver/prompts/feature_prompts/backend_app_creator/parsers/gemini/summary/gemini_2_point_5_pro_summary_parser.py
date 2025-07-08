9a891127372f1c800ba3eafc055cabb3fefec156
Satyam Kumar <satyam.kumar1@1mg.com>
2025-07-08 17:18:45 +0530

    Prompt changes for backend app

---
+++ app/main/blueprints/one_dev/services/query_solver/prompts/feature_prompts/backend_app_creator/parsers/gemini/summary/gemini_2_point_5_pro_summary_parser.py
@@ -0,0 +1,7 @@
+from app.main.blueprints.one_dev.services.query_solver.prompts.feature_prompts.backend_app_creator.parsers.gemini.summary.base_summary_parser import (
+    SummaryParser,
+)
+
+
+class Gemini2Point5ProSummaryParser(SummaryParser):
+    pass
