867855d22be68db690b0786ddb6375e10d4e7d5e
Vaibhav Meena <vaibhav.meena@1mg.com>
2025-07-11 16:34:20 +0530

    Add optional Cache-Control header to presigned URL generation and update config

--- app/backend_common/services/binary_file_upload/binary_file_upload.py
+++ app/backend_common/services/binary_file_upload/binary_file_upload.py
@@ -1,7 +1,6 @@
 from deputydev_core.utils.config_manager import ConfigManager
 
-from app.backend_common.service_clients.aws.services.s3 import \
-    AWSS3ServiceClient
+from app.backend_common.service_clients.aws.services.s3 import AWSS3ServiceClient
 
 
 class BinaryFileUpload:
@@ -10,9 +9,9 @@ class BinaryFileUpload:
         region_name=ConfigManager.configs["AWS_S3_BINARY_BUCKET"]["AWS_REGION"],
     )
 
-    PRESIGNED_URL_EXPIRY = ConfigManager.configs["BINARY_UPLOAD"][
-        "PRESIGNED_URL_EXPIRY"
-    ]
+    PRESIGNED_URL_EXPIRY = ConfigManager.configs["BINARY_UPLOAD"]["PRESIGNED_URL_EXPIRY"]
+
+    CACHE_CONTROL = ConfigManager.configs["BINARY_UPLOAD"]["CACHE_CONTROL"]
 
     @classmethod
     async def get_presigned_urls_for_upload(cls, s3_key: str) -> str:
@@ -20,6 +19,6 @@ class BinaryFileUpload:
         Generate presigned URLs for uploading binary, to be given to the client
         """
         presigned_post_url = await cls.s3_client.create_presigned_put_url(
-            expiry=cls.PRESIGNED_URL_EXPIRY, s3_key=s3_key
+            expiry=cls.PRESIGNED_URL_EXPIRY, s3_key=s3_key, cache_control=cls.CACHE_CONTROL
         )
         return presigned_post_url
