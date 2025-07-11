fa85692bcc5aad9195c3ed68b40d7dfa6add5418
Vaibhav Meena <vaibhav.meena@1mg.com>
2025-07-11 17:46:05 +0530

    minor fix

--- app/main/blueprints/one_dev/routes/end_user/v1/binary_upload/binary_upload_blueprint.py
+++ app/main/blueprints/one_dev/routes/end_user/v1/binary_upload/binary_upload_blueprint.py
@@ -1,17 +1,18 @@
+from typing import Any
+
+from deputydev_core.utils.config_manager import ConfigManager
 from sanic import Blueprint
 from torpedo import Request, send_response
-from typing import Any
 from torpedo.exceptions import HTTPRequestException
-from deputydev_core.utils.config_manager import ConfigManager
-from app.backend_common.services.binary_file_upload.binary_file_upload import BinaryFileUpload
 
+from app.backend_common.services.binary_file_upload.binary_file_upload import BinaryFileUpload
 
 binary_upload_v1_bp = Blueprint("binary_upload_v1_bp", url_prefix="/binary-upload")
 
 
 @binary_upload_v1_bp.route("/get-presigned-url", methods=["POST"])
-async def get_presigned_url(_request: Request, **kwargs: Any):
-    BINARY_UPLOAD_AUTH_KEY = ConfigManager.configs["BINARY_UPLOAD"]["AUTH_KEY"]
+async def get_presigned_url(_request: Request, **kwargs: Any) -> dict[str, Any]:
+    BINARY_UPLOAD_AUTH_KEY = ConfigManager.configs["BINARY_UPLOAD"]["AUTH_KEY"]  # noqa: N806
     client_key = _request.headers.get("authorization")
     if client_key != BINARY_UPLOAD_AUTH_KEY:
         raise HTTPRequestException("Invalid API key", status_code=401)
@@ -22,7 +23,7 @@ async def get_presigned_url(_request: Request, **kwargs: Any):
             raise HTTPRequestException("s3_key is required")
         presigned_url_for_binary_upload = await BinaryFileUpload.get_presigned_urls_for_upload(s3_key)
         return send_response({presigned_url_for_binary_upload: presigned_url_for_binary_upload})
-    except Exception as _ex:
+    except Exception as _ex:  # noqa: BLE001
         raise HTTPRequestException(
             f"Error generating presigned URL: {_ex}",
         )
