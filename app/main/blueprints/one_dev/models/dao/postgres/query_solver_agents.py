7bdb07288d414fe7159a2487833840f2301dfc1e
Satyam Kumar <satyam.kumar1@1mg.com>
2025-07-14 12:13:37 +0530

    Add query_solver_agents table

---
+++ app/main/blueprints/one_dev/models/dao/postgres/query_solver_agents.py
@@ -0,0 +1,29 @@
+from tortoise import fields
+
+from app.backend_common.models.dao.postgres.base import Base
+
+
+class QuerySolverAgent(Base):
+    serializable_keys = {
+        "id",
+        "name",
+        "unique_slug",
+        "description",
+        "prompt_intent",
+        "allowed_first_party_tools",
+        "status",
+        "created_at",
+        "updated_at",
+    }
+
+    id = fields.IntField(primary_key=True)
+    name = fields.TextField()
+    agent_enum = fields.TextField()
+    description = fields.TextField()
+    prompt_intent = fields.TextField()
+    status = fields.TextField(default="ACTIVE")
+    allowed_first_party_tools = fields.JSONField()
+
+    class Meta:
+        table = "query_solver_agents"
+        indexes = (("unique_slug",),)
