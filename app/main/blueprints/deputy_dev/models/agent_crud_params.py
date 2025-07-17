a5c817a6c7800e09ce33012489c797949cc4febf
sachendra jain <sachendra.jain@1mg.com>
2025-07-18 00:34:18 +0530

    agents apis

--- app/main/blueprints/deputy_dev/models/agent_crud_params.py
+++ app/main/blueprints/deputy_dev/models/agent_crud_params.py
@@ -7,8 +7,3 @@ class AgentParams(BaseModel):
     name: str
     custom_prompt: str
     user_team_id: int
-    confidence_score: Optional[float] = 0.9
-    is_custom_agent: Optional[bool] = True
-    exclusions: Optional[list] = []
-    inclusions: Optional[list] = []
-    objective: Optional[str] = ""
--- db/migrations/20250709140000_create_user_agents.sql
+++ db/migrations/20250709140000_create_user_agents.sql
@@ -8,11 +8,16 @@ CREATE TABLE IF NOT EXISTS user_agents (
     exclusions JSONB DEFAULT '[]',
     inclusions JSONB DEFAULT '[]',
     confidence_score FLOAT DEFAULT 0.9,
-    objective TEXT DEFAULT 'Responsibility of this agent is checking security issues',
+    objective TEXT DEFAULT '',
     is_custom_agent BOOLEAN DEFAULT FALSE,
+    is_deleted BOOLEAN DEFAULT FALSE,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
 );
+
+CREATE UNIQUE INDEX IF NOT EXISTS user_agents_user_team_id_agent_name_unique_active
+ON user_agents (user_team_id, agent_name)
+WHERE is_deleted = FALSE;
 
 -- migrate:down
 DROP TABLE IF EXISTS user_agents;
