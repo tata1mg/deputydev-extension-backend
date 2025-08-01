d1a38e9bc24fedab921e749d7fa314e201964f03
Your Name <ankit.rana1@1mg.com>
2025-08-01 16:11:07 +0530

    ruff

--- app/main/blueprints/one_dev/services/kafka/analytics_events/dataclasses/kafka_analytics_events.py
+++ app/main/blueprints/one_dev/services/kafka/analytics_events/dataclasses/kafka_analytics_events.py
@@ -35,9 +35,9 @@ class KafkaAnalyticsEventMessage(BaseModel):
 
     # add validation to only allow event_type values in SNAKE_CASE
     @field_validator("event_type")
-    def event_type_must_be_snake_case(cls, v: str) -> str:
+    def event_type_must_be_snake_case(cls, v: EventTypes) -> str:
         # Allow only CAPITAL_SNAKE_CASE (uppercase letters and underscores, cannot start/end with underscore, no consecutive underscores)
         capital_snake_case_pattern = r"^[A-Z]+(_[A-Z]+)*$"
-        if not re.match(capital_snake_case_pattern, v):
+        if not re.match(capital_snake_case_pattern, v.value):
             raise ValueError("event_type must be in CAPITAL_SNAKE_CASE (UPPERCASE with underscores)")
         return v
