f4a461c16641f8e321874c4a800fc0cf70abd3c7
Satyam Kumar <satyam.kumar1@1mg.com>
2025-08-05 16:23:51 +0530

    Add ability to save tool response in new table

--- app/backend_common/services/llm/providers/openai/llm_provider.py
+++ app/backend_common/services/llm/providers/openai/llm_provider.py
@@ -81,7 +81,7 @@ class OpenAI(BaseLLMProvider):
         cache_config: PromptCacheConfig = PromptCacheConfig(tools=False, system_message=False, conversation=False),
         search_web: bool = False,
         disable_caching: bool = False,
-        previous_conversation_turns: List[ConversationTurn] = []
+        previous_conversation_turns: List[ConversationTurn] = [],
     ) -> Dict[str, Any]:
         """
         Formats the conversation for OpenAI's GPT model.
