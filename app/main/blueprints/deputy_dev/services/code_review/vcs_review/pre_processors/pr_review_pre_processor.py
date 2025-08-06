d84b32820d11294d68ec8fc598cf71fa94ed310a
Satyam Kumar <satyam.kumar1@1mg.com>
2025-08-06 20:48:40 +0530

    Fix CQA

--- app/main/blueprints/deputy_dev/services/code_review/vcs_review/pre_processors/pr_review_pre_processor.py
+++ app/main/blueprints/deputy_dev/services/code_review/vcs_review/pre_processors/pr_review_pre_processor.py
@@ -1,5 +1,9 @@
 from typing import Any, Dict, Optional
 
+from deputydev_core.utils.app_logger import AppLogger
+from deputydev_core.utils.constants.enums import Clients, ContextValueKeys
+from deputydev_core.utils.context_value import ContextValue
+from deputydev_core.utils.context_vars import get_context_value, set_context_values
 from torpedo import CONFIG
 
 from app.backend_common.constants.constants import LARGE_PR_DIFF, PR_NOT_FOUND, PRStatus
@@ -43,10 +47,6 @@ from app.main.blueprints.deputy_dev.services.repository.pr.pr_service import PRS
 from app.main.blueprints.deputy_dev.services.setting.setting_service import (
     SettingService,
 )
-from deputydev_core.utils.app_logger import AppLogger
-from deputydev_core.utils.constants.enums import Clients, ContextValueKeys
-from deputydev_core.utils.context_value import ContextValue
-from deputydev_core.utils.context_vars import get_context_value, set_context_values
 
 config = CONFIG.config
 one_dev_review_client = OneDevReviewClient()
