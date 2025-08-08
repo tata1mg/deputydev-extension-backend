89d580be7a00da7e55aaffaa53efbd0eb31684a6
Satyam Kumar <satyam.kumar1@1mg.com>
2025-08-08 11:36:38 +0530

    Remove useless code

--- app/main/blueprints/one_dev/services/code_generation/feedback/dataclasses/main.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-from app.backend_common.constants.feedbacks import UpvoteDownvoteFeedbacks
-
-
-class CodeGenerationFeedbackInput(BaseModel):
-    feedback: UpvoteDownvoteFeedbacks
-    job_id: int
