38061fc6234f31e95d1fb4b1aa1ccf5d824e71e6
Satyam Kumar <satyam.kumar1@1mg.com>
2025-08-19 12:32:40 +0530

    Fixes

--- app/main/blueprints/one_dev/services/history/code_gen_agent_chats/code_gen_agent_chats_manager.py
+++ app/main/blueprints/one_dev/services/history/code_gen_agent_chats/code_gen_agent_chats_manager.py
@@ -1,6 +1,13 @@
 from typing import List
 
-from app.main.blueprints.one_dev.models.dto.agent_chats import AgentChatDTO
+from app.backend_common.services.chat_file_upload.chat_file_upload import ChatFileUpload
+from app.backend_common.services.chat_file_upload.dataclasses.chat_file_upload import Attachment
+from app.main.blueprints.one_dev.models.dto.agent_chats import (
+    AgentChatDTO,
+    TextMessageData,
+    ToolStatus,
+    ToolUseMessageData,
+)
 from app.main.blueprints.one_dev.services.history.code_gen_agent_chats.dataclasses.code_gen_agent_chats import (
     ChatElement,
 )
@@ -13,9 +20,39 @@ class PastCodeGenAgentChatsManager:
     """
 
     @classmethod
-    async def get_serialized_previous_chat_data(cls, raw_agent_chats: List[AgentChatDTO]) -> List[ChatElement]:
+    async def get_serialized_previous_chat_data(cls, raw_agent_chats: List[AgentChatDTO]) -> List[ChatElement]:  # noqa: C901
+        all_attachments: List[Attachment] = []
+        for chat_data in raw_agent_chats:
+            if isinstance(chat_data.message_data, TextMessageData):
+                all_attachments.extend(chat_data.message_data.attachments)
+
+        attachment_data_task_map = ChatFileUpload.get_attachment_data_task_map(all_attachments=all_attachments)
         all_elements: List[ChatElement] = []
         for chat_data in raw_agent_chats:
+            if isinstance(chat_data.message_data, TextMessageData):
+                if chat_data.message_data.directory_items:
+                    for item in chat_data.message_data.directory_items:
+                        item.structure = None
+
+                if chat_data.message_data.focus_items:
+                    for item in chat_data.message_data.focus_items:
+                        item.chunks = []
+
+                if chat_data.message_data.attachments:
+                    for item in chat_data.message_data.attachments:
+                        item.attachment_data = (
+                            (await attachment_data_task_map[item.attachment_id])
+                            if item.attachment_id in attachment_data_task_map
+                            else None
+                        )
+
+            if isinstance(chat_data.message_data, ToolUseMessageData):
+                chat_data.message_data.tool_status = (
+                    ToolStatus.ABORTED
+                    if chat_data.message_data.tool_status == ToolStatus.PENDING
+                    else chat_data.message_data.tool_status
+                )
+
             element = ChatElement(
                 actor=chat_data.actor,
                 message_data=chat_data.message_data,
