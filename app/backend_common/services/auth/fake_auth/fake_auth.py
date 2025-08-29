c1c7043c9bc2273750631e025173c19b6cd63137
Pratham verma <pratham.verma@1mg.com>
2025-08-29 16:25:46 +0530

    auth session data typed fix

--- app/backend_common/services/auth/fake_auth/fake_auth.py
+++ app/backend_common/services/auth/fake_auth/fake_auth.py
@@ -1,25 +1,26 @@
-from typing import Any, Dict
+from typing import Dict
 
 from deputydev_core.utils.config_manager import ConfigManager
 from deputydev_core.utils.constants.auth import AuthStatus
 from torpedo import Request
 
 from app.backend_common.services.auth.base_auth import BaseAuth
+from app.backend_common.utils.dataclasses.main import AuthSessionData
 
 
 class FakeAuth(BaseAuth):
-    async def get_auth_session(self, headers: Dict[str, str]) -> Dict[str, Any]:
-        return {
-            "status": AuthStatus.AUTHENTICATED.value,
-            "user_email": ConfigManager.configs["FAKE_AUTH"]["USER_EMAIL"],
-            "user_name": ConfigManager.configs["FAKE_AUTH"]["USER_NAME"],
-            "encrypted_session_data": ConfigManager.configs["FAKE_AUTH"]["ENCRYPTED_SESSION_DATA"],
-        }
+    async def get_auth_session(self, headers: Dict[str, str]) -> AuthSessionData:
+        return AuthSessionData(
+            status=AuthStatus.AUTHENTICATED,
+            user_email=ConfigManager.configs["FAKE_AUTH"]["USER_EMAIL"],
+            user_name=ConfigManager.configs["FAKE_AUTH"]["USER_NAME"],
+            encrypted_session_data=ConfigManager.configs["FAKE_AUTH"]["ENCRYPTED_SESSION_DATA"],
+        )
 
-    async def extract_and_verify_token(self, request: Request) -> Dict[str, Any]:
-        return {
-            "status": AuthStatus.VERIFIED.value,
-            "user_email": ConfigManager.configs["FAKE_AUTH"]["USER_EMAIL"],
-            "user_name": ConfigManager.configs["FAKE_AUTH"]["USER_NAME"],
-            "encrypted_session_data": ConfigManager.configs["FAKE_AUTH"]["ENCRYPTED_SESSION_DATA"],
-        }
+    async def extract_and_verify_token(self, request: Request) -> AuthSessionData:
+        return AuthSessionData(
+            status=AuthStatus.VERIFIED,
+            user_email=ConfigManager.configs["FAKE_AUTH"]["USER_EMAIL"],
+            user_name=ConfigManager.configs["FAKE_AUTH"]["USER_NAME"],
+            encrypted_session_data=ConfigManager.configs["FAKE_AUTH"]["ENCRYPTED_SESSION_DATA"],
+        )
