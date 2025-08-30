6eedfc295a458cc9f418c675306fcf53a688d1cc
Pratham verma <pratham.verma@1mg.com>
2025-08-30 11:11:36 +0530

    minor change

--- app/backend_common/services/auth/auth_factory.py
+++ app/backend_common/services/auth/auth_factory.py
@@ -9,8 +9,33 @@ from app.backend_common.utils.dataclasses.main import AuthProvider
 
 
 class AuthFactory:
+    """
+    A factory class that provides authentication provider instances.
+
+    This factory is responsible for creating and returning the appropriate authentication
+    provider instance based on the application's configuration and requested provider type.
+    It supports multiple authentication providers and can be easily extended to support more.
+    """
+
     @staticmethod
     def get_auth_provider(auth_provider: AuthProvider = AuthProvider.SUPABASE) -> BaseAuth:
+        """
+        Get an instance of the requested authentication provider.
+
+        Args:
+            auth_provider (AuthProvider, optional): The type of authentication provider to create.
+                Defaults to AuthProvider.SUPABASE.
+
+        Returns:
+            BaseAuth: An instance of the requested authentication provider.
+
+        Raises:
+            ValueError: If the specified provider is not supported.
+
+        Note:
+            If FAKE_AUTH is enabled in the configuration, this method will always return
+            a FakeAuth instance regardless of the requested provider.
+        """
         auth_providers: Dict[AuthProvider, Type[BaseAuth]] = {
             AuthProvider.SUPABASE: SupabaseAuth,
             AuthProvider.FAKEAUTH: FakeAuth,
