8a453125f3a6734f7a2fbd470b6085081da24154
Pratham verma <pratham.verma@1mg.com>
2025-09-04 20:27:27 +0530

    auth service host integration

--- app/backend_common/utils/authenticate.py
+++ app/backend_common/utils/authenticate.py
@@ -1,91 +1,56 @@
 import json
-from datetime import datetime, timezone
 from functools import wraps
 from typing import Any, Dict, Tuple
 
-from deputydev_core.utils.constants.auth import AuthStatus
+import aiohttp
+from deputydev_core.utils.config_manager import ConfigManager
 from deputydev_core.utils.context_value import ContextValue
 from sanic.server.websockets.impl import WebsocketImplProtocol
 
-from app.backend_common.constants.onboarding import SubscriptionStatus
-from app.backend_common.models.dto.user_team_dto import UserTeamDTO
-from app.backend_common.repository.subscriptions.repository import SubscriptionsRepository
-from app.backend_common.repository.user_teams.user_team_repository import (
-    UserTeamRepository,
-)
-from app.backend_common.repository.users.user_repository import UserRepository
-from app.backend_common.utils.sanic_wrapper import CONFIG, Request
+from app.backend_common.utils.dataclasses.main import AuthData, ClientData
+from app.backend_common.utils.sanic_wrapper import Request
 from app.backend_common.utils.sanic_wrapper.exceptions import BadRequestException
-from app.backend_common.services.auth.auth_factory import AuthFactory
-from app.backend_common.services.auth.signup import SignUp
-from app.backend_common.utils.dataclasses.main import AuthData, AuthSessionData, ClientData
 
 
 async def get_auth_data(request: Request) -> Tuple[AuthData, Dict[str, Any]]:  # noqa: C901
     """
-    Get the auth data from the request
+    Get the auth data by calling the auth service API using aiohttp
     """
-    response_headers = {}
-    auth_provider = AuthFactory.get_auth_provider()
-    verification_result: AuthSessionData = await auth_provider.extract_and_verify_token(request)
+    use_grace_period: bool = False
+    enable_grace_period: bool = False
+    payload: Dict[str, Any] = {}
 
-    if verification_result.status == AuthStatus.NOT_VERIFIED:
-        raise BadRequestException(verification_result.error_message or AuthStatus.NOT_VERIFIED.value)
+    authorization_header: str = request.headers.get("Authorization")
+    if not authorization_header:
+        raise BadRequestException("Authorization header is missing")
 
-    if verification_result.status == AuthStatus.EXPIRED:
-        response_headers = {"new_session_data": verification_result.encrypted_session_data}
+    try:
+        payload = request.custom_json() if request.method == "POST" else request.request_params()
+        use_grace_period = payload.get("use_grace_period") or False
+        enable_grace_period = payload.get("enable_grace_period") or False
+    except Exception:  # noqa: BLE001
+        pass
 
-    # Extract the email from the user response
-    email = verification_result.user_email
-    if not email:
-        raise BadRequestException("Email not found in session data")
+    dd_auth_service_host = ConfigManager.configs["DD_AUTH_SERVICE_HOST"]
+    dd_get_auth_endpoint = ConfigManager.config["DD_AUTH_SERVICE_GET_AUTH_DATA_ENDPOINT"]
+    auth_service_url = f"{dd_auth_service_host}{dd_get_auth_endpoint}"
 
-    # Fetch the user ID based on the email
-    user = await UserRepository.db_get(filters={"email": email}, fetch_one=True)
-    user_id = user.id
+    headers = {"Authorization": authorization_header}
 
-    # If the user ID is not found, raise an error
-    if not user_id:
-        raise BadRequestException("User not found")
+    params = {
+        "use_grace_period": str(use_grace_period).lower(),
+        "enable_grace_period": str(enable_grace_period).lower(),
+    }
 
-    # Get the team ID based on the email domain
-    team_info = await SignUp.get_team_info_from_email(email)
-    team_id = team_info.get("team_id")
-
-    # If the team ID is not found, raise an error
-    if not team_id:
-        raise BadRequestException("Team not found")
-
-    # Fetch the user team ID based on the user ID and team ID
-    user_team: UserTeamDTO = await UserTeamRepository.db_get(
-        filters={"user_id": user_id, "team_id": team_id}, fetch_one=True
-    )
-    user_team_id = user_team.id
-
-    # If the user team ID is not found, raise an error
-    if not user_team_id:
-        raise BadRequestException("User team not found")
-
-    # Check subscription
-    subscription = await SubscriptionsRepository.get_by_user_team_id(user_team_id)
-    if not subscription:
-        raise BadRequestException("Subscription not found")
-
-    # Check subscription expiry
-    if subscription.end_date is not None and (
-        subscription.end_date < datetime.now(timezone.utc)
-        or SubscriptionStatus(subscription.current_status) != SubscriptionStatus.ACTIVE
-    ):
-        raise BadRequestException("Subscription expired")
-
-    # prepare the auth data
-    auth_data = None
-    if response_headers and response_headers["new_session_data"]:
-        auth_data = AuthData(user_team_id=user_team_id, session_refresh_token=response_headers["new_session_data"])
-    else:
-        auth_data = AuthData(user_team_id=user_team_id)
-
-    return auth_data, response_headers
+    async with aiohttp.ClientSession() as session:
+        response = await session.get(auth_service_url, headers=headers, params=params)
+        response_data = await response.json()
+        if response_data:
+            auth_data = AuthData(**response_data.get("auth_data"))
+            response_headers = response_data.get("response_headers", {})
+            return auth_data, response_headers
+        else:
+            raise BadRequestException("Auth Data is missing")
 
 
 def authenticate(func: Any) -> Any:
