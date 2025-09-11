2a74132af82445be74c580fffcae085ba0defd63
Satyam Kumar <satyam.kumar1@1mg.com>
2025-09-11 11:09:14 +0530

    Remove VCS review code

--- app/backend_common/services/credentials/confluence_auth_handler.py
+++
@@ -1,8 +0,0 @@
-from __future__ import annotations
-
-from .atlassian_auth_handler import AtlassianAuthHandler
-
-
-class ConfluenceAuthHandler(AtlassianAuthHandler):
-    __integration__ = "confluence"
-    __tokenable_type__ = "integration"
