4786f499936e9637ecf22d79ca5166550e5eebf2
Satyam Kumar <satyam.kumar1@1mg.com>
2025-09-25 14:14:03 +0530

    Fix double decoding

--- app/backend_common/services/openai/openai_service.py
+++ app/backend_common/services/openai/openai_service.py
@@ -132,9 +132,9 @@ class OpenAIManager(BaseClient):
             embeddings[index] = new_embeddings[i]
 
         try:
-            for i in range(0, len(cache_keys), 25):
-                batch_keys = cache_keys[i : i + 25]
-                batch_embeddings = embeddings[i : i + 25]
+            for i in range(0, len(cache_keys), 100):
+                batch_keys = cache_keys[i : i + 100]
+                batch_embeddings = embeddings[i : i + 100]
 
                 # Store the current batch in Redis
                 await CommonCache.mset_with_expire(
