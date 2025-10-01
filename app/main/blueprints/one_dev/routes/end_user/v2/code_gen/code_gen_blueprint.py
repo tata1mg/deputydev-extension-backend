c5ba5a52029d1d40cad14f440c762dacfe975742
Satyam Kumar <satyam.kumar1@1mg.com>
2025-10-01 12:40:10 +0530

    Use stream handler in query solver

--- app/main/blueprints/one_dev/routes/end_user/v2/code_gen/code_gen_blueprint.py
+++ app/main/blueprints/one_dev/routes/end_user/v2/code_gen/code_gen_blueprint.py
@@ -119,16 +119,15 @@ async def cancel_chat(
 @validate_client_version
 @authenticate
 @ensure_session_id(auto_create=True)
-async def solve_user_query_ws(_request: Request, ws: WebsocketImplProtocol, **kwargs: Any) -> None:  # noqa: C901
-    client_data: ClientData = kwargs.get("client_data")
-    auth_data: AuthData = kwargs.get("auth_data")
-    session_id: int = kwargs.get("session_id")
+async def solve_user_query_ws(_request: Request, ws: WebsocketImplProtocol, client_data: ClientData, auth_data: AuthData, session_id: int) -> None:
+
+    # TODO: Remove after MessageSession deprecation
     session_type: str = _request.headers.get("X-Session-Type")
 
     # Initialize query solver
     query_solver = QuerySolver()
 
-    # Start a background task to send pings every 25 seconds
+    # Start a background task to send pings every 25 seconds to keep the websocket alive
     pinger_task = None
     try:
         pinger_task = asyncio.create_task(query_solver.start_pinger(ws))
