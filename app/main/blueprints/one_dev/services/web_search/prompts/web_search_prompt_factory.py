4ac816ad6f9e0baaafe4bab7a7ac0a8c1d418165
Vaibhav Meena <vaibhav.meena@1mg.com>
2025-11-06 17:27:52 +0530

    minor cjanges

--- app/main/blueprints/one_dev/services/web_search/prompts/web_search_prompt_factory.py
+++ app/main/blueprints/one_dev/services/web_search/prompts/web_search_prompt_factory.py
@@ -4,14 +4,14 @@ from deputydev_core.llm_handler.models.dto.message_thread_dto import LLModels
 from deputydev_core.llm_handler.prompts.base_feature_prompt_factory import BaseFeaturePromptFactory
 from deputydev_core.llm_handler.prompts.base_prompt import BasePrompt
 
-from app.main.blueprints.one_dev.services.web_search.prompts.gemini_2_point_5_flash import (
-    Gemini2Point5FlashWebSearch,
+from app.main.blueprints.one_dev.services.web_search.prompts.gemini_2_point_0_flash import (
+    Gemini2Point0FlashWebSearch,
 )
 
 
 class WebSearchPromptFactory(BaseFeaturePromptFactory):
     web_search_prompts = {
-        LLModels.GEMINI_2_POINT_5_FLASH: Gemini2Point5FlashWebSearch,
+        LLModels.GEMINI_2_POINT_0_FLASH: Gemini2Point0FlashWebSearch,
     }
 
     @classmethod
